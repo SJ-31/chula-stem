d8e2bb9d255107f7554eca5b277defeabfe860ae
Shann Chongwattanankul <shanj3131@gmail.com>
2024-11-18 17:20:55 +0700

    feat: add processes for delly cnv analysis

---
+++ nextflow/modules/delly_cnv.nf
@@ -0,0 +1,33 @@
+process DELLY_CNV {
+    ext version: "1.3.1"
+ 
+    publishDir "$meta.out", mode: "copy", saveAs: { x -> x ==~ /.*\.log/ ? null : x }
+    publishDir "$meta.log", mode: "copy", pattern: "*.log"
+
+    input:
+    tuple val(meta), path(normal), path(tumor), path(indices, arity: "2"), path(covfile)
+    val(reference)
+    val(mapfile)
+    val(module_number)
+    //
+
+    output:
+    tuple val(meta), path(out)
+    path("*.log")
+    //
+
+    shell:
+    out = "${module_number}-${meta.id}_DellyCNV.vcf.gz"
+    // the output of this is yet
+    check = file("${meta.out}/${out}")
+    args = task.ext.args.join(" ")
+    if (check.exists()) {
+        '''
+        ln -sr !{check} .
+        ln -sr !{meta.log}/dellyCNV.log .
+        '''
+    } else {
+        template 'delly_cnv.bash'
+    }
+    //
+}
---
+++ nextflow/modules/templates/delly_cnv.bash
@@ -0,0 +1,44 @@
+get_sample_file () {
+    normal_sample=!{meta.RGSM_normal}
+    tumor_sample=$(bcftools query -l "$1" | sed "s/$normal_sample//" | xargs)
+    echo -e "${normal_sample}\tcontrol" > samples.tsv
+    echo -e "${tumor_sample}\ttumor" >> samples.tsv
+}
+
+# Segment tumor genome
+# TODO: can't fill these parameters until you have software that estimates ploidy and purity
+# purity=$()
+# ploidy=
+delly cnv --segmentation \
+    --cnv-size "TODO" \
+    --cn_offset "TODO" \
+    --sdrd "TODO" \
+    --purity "TODO" \
+    --outfile tumor.bcf \
+    --covfile !{covfile} \
+    --genome !{reference} \
+    --mappability !{mapfile} \
+    !{tumor}
+# Detection sensitivity is controlled by
+# --cnv-size (-z):
+# --cn_offset (-t):
+#   Is the minimum copy-number shift for segmentation and somatic classification
+#   Can calculate as purity * ploidy + 2 (diploid number) * (1-purity)
+# --sdrd (read-depth shift, -x):
+#
+# which should be determined dynamically for each tumor sample
+
+# Genotype somatic CNVs in normal sample
+delly cnv --segmentation \
+    --vcffile tumor.bcf \
+    --outfile normal.bcf \
+    --genome !{reference} \
+    --mappability \
+    !{normal}
+
+bcftools merge -m id -O b -o tmp.bcf tumor.bcf normal.bcf
+get_sample_file tmp.bcf
+
+delly classify --pass --filter somatic --outfile !{out} -s samples.tsv tmp.bcf
+
+get_nextflow_log.bash dellyCNV.log
