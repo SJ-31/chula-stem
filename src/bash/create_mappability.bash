9cfe4921a4ca1dd8b7a8545d50df5e1170ef63ee
Shann Chongwattanankul <shanj3131@gmail.com>
2024-11-20 17:21:38 +0700

    chore: correct param errors in mappability

--- src/bash/create_mappability.bash
+++ src/bash/create_mappability.bash
@@ -6,7 +6,16 @@ reference="$1" # Genome fasta file
 kmer="$2" # Kmer size should be the length of mapped reads
 pref=${reference/\.*/}
 threads=4
-gem-indexer -T ${threads} -c dna -i "${reference}" -o "${pref}"_index
-gem-mappability -T ${threads} -i "${pref}_index.gem" -l "${kmer}" \
--o "${pref}_${kmer}" -m 2 -e 2
-gem-2-wig -i "${pref}_index.gem" -i "${pref}_${kmer}.mappability" \
+
+gem-indexer -T ${threads} -c dna \
+   -i "${reference}" \
+   -o "${pref}"_index
+
+gem-mappability -T ${threads} \
+    -I "${pref}_index.gem" \
+    -l "${kmer}" \
+    -o "${pref}_${kmer}" -m 2 -e 2
+
+gem-2-wig -I "${pref}_index.gem" \
+    -i "${pref}_${kmer}.mappability" \
+    -o "${pref}_${kmer}"
