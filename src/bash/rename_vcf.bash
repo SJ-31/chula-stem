c265668465b1e5d130056a9d1c3644a7e939a20f
Shann Chongwattanankul <shanj3131@gmail.com>
2024-11-25 15:34:05 +0700

    chore: allow reversing of vcf name

--- src/bash/rename_vcf.bash
+++ src/bash/rename_vcf.bash
@@ -3,7 +3,8 @@
 # Rename samples in the provided paired-normal vcf file to NORMAL, TUMOR and reorder
 # the vcf to be NORMAL\tTUMOR
 #
-while getopts "i:o:n:t:" f; do
+reverse=0
+while getopts "vi:o:n:t:" f; do
     case "$f" in
         i)
             input="${OPTARG}"
@@ -17,12 +18,23 @@ while getopts "i:o:n:t:" f; do
         t)
             tumor_name="${OPTARG}"
             ;;
+        v)
+            reverse=1
+            ;;
         *)
             echo "Unknown flag"
             exit 1
     esac
 done
 
-echo -e "${normal_name} NORMAL\n${tumor_name} TUMOR" > rename.txt
+if [[ ${reverse} == 0 ]]; then
+    rename_str="${normal_name} NORMAL\n${tumor_name} TUMOR"
+    order_str="NORMAL,TUMOR"
+else
+    rename_str="NORMAL ${normal_name}\nTUMOR ${tumor_name}"
+    order_str="${normal_name},${tumor_name}"
+fi
+
+echo -e "${rename_str}" > rename.txt
 bcftools reheader -s rename.txt "${input}" | \
-    bcftools view -s NORMAL,TUMOR -O z > "${output}"
+    bcftools view -s "${order_str}" -O z > "${output}"
