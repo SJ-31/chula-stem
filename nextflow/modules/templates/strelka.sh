a0e548f6bb12362ca375763f3f372377adec8cc4
Shann Chongwattanankul <shanj3131@gmail.com>
2024-11-27 11:25:12 +0700

    bug: do not process manta vcf in strelka

--- nextflow/modules/templates/strelka.sh
+++ nextflow/modules/templates/strelka.sh
@@ -10,28 +10,26 @@ configureStrelkaSomaticWorkflow.py \
     --runDir !{out}
 
 !{out}/runWorkflow.py -m local
-# Memory options/cores shouled be controlled by SLURM so don't need to be specified
+# Memory options/cores should be controlled by SLURM so don't need to be specified
 
 mv !{out}/results/variants/*.vcf.gz .
-for variant in *.vcf.gz; do
-    if [[ ! "$variant" == "candidateSmallIndels.vcf.gz" ]]; then
-        base=$(echo $variant | sed 's/\.vcf\.gz//')
-        name="!{module_number}-${base}_Strelka.vcf"
+for variant in somatic*.vcf.gz; do
+    base=$(echo $variant | sed 's/\.vcf\.gz//')
+    name="!{module_number}-${base}_Strelka.vcf"
 
-        rename_vcf.bash -v -i $variant -o tmp.vcf.gz \
-            -n "${meta.RGSM_normal}" -t "${meta.RGSM_tumor}"
+    rename_vcf.bash -v -i $variant -o tmp.vcf.gz \
+        -n "!{meta.RGSM_normal}" -t "!{meta.RGSM_tumor}"
 
-        vcf_info_add_tag -n SOURCE \
-            -d "!{params.source_description}" \
-            -b '.' \
-            -t String \
-            -a strelka2 \
-            -i tmp.vcf.gz \
-            -o "${name}"
+    vcf_info_add_tag -n SOURCE \
+        -d "!{params.source_description}" \
+        -b '.' \
+        -t String \
+        -a strelka2 \
+        -i tmp.vcf.gz \
+        -o "${name}"
 
-        rm "$variant"
-        bgzip "${name}"
-    fi
+    rm "$variant"
+    bgzip "${name}"
 done
 
 get_nextflow_log.bash strelka.log
