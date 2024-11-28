66602105ece95f9a39503baf29123f724db9789f
Shann Chongwattanankul <shanj3131@gmail.com>
2024-11-28 15:01:20 +0700

    feat: better version of add tag

---
+++ src/bash/vcf_info_add_tag.bash
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# If a letter is followed by a colon, the option expects an argument
+while getopts "i:o:b:n:d:t:a:" f; do
+    case "$f" in
+        i)
+            input=${OPTARG}
+            ;;
+        o)
+            output=${OPTARG}
+            ;;
+        b)
+            number=${OPTARG}
+            ;;
+        n)
+            name=${OPTARG}
+            ;;
+        d)
+            description=${OPTARG}
+            ;;
+        t)
+            type=${OPTARG}
+            ;;
+        a)
+            default=${OPTARG}
+            ;;
+        *)
+            echo "Flag not provided"
+            exit 1
+    esac
+done
+
+echo "##INFO=<ID=${name},Number=${number},Type=${type},Description=\"${description}\">" > line.txt
+
+bcftools annotate -h line.txt "${input}" | \
+    awk -v tn="${name}" -v val="${default}" \
+    'BEGIN {OFS="\t"} {if ($1 !~ "^#") {$8=$8";"tn"="val} {print}}' | \
+    bcftools view -O z > "${output}"
+
+rm line.txt
