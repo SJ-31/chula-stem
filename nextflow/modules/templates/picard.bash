4d38ff4da93a090e017c3e9319317b87b3bb15e8
Shann Chongwattanankul <shanj3131@gmail.com>
2024-12-02 17:33:24 +0700

    bug: fix picard typos

--- nextflow/modules/templates/picard.bash
+++ nextflow/modules/templates/picard.bash
@@ -4,15 +4,15 @@ if [[ "!{omics_type}" == "hs" ]]; then
     gatk CollectHsMetrics -I "!{bam}" \
         --BAIT_INTERVALS "!{bait_intervals}" \
         --TARGET_INTERVALS "!{target_intervals}" \
-        -O "!{exome}"
+        -O "!{out2}"
 elif [[ "!{omics_type}" == "wgs" ]]; then
     gatk CollectWgsMetrics -I "!{bam}" \
         --REFERENCE_SEQUENCE "!{reference}" \
-        -O "!{wgs}"
+        -O "!{out2}"
 elif [[ "!{omics_type}" == "rnaseq" ]]; then
     gatk CollectRnaSeqMetrics -I !{}bam \
         --REF_FLAT "!{gene_annotations_refFlat}" \
-        -O "!{rnaseq}"
+        -O "!{out2}"
 else
     echo "'omics_type' must be one of wgs|rna-seq|exome"
     exit 1
