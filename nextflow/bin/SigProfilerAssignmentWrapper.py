718a0e7ce694bac1f332f71de0ffc36ec8b9d706
Shann Chongwattanankul <shanj3131@gmail.com>
2024-12-09 12:47:07 +0700

    feat: add sigprofilerassignment for mutational signatures

---
+++ nextflow/bin/SigProfilerAssignmentWrapper.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from SigProfilerAssignment import Analyzer as Analyze
+
+
+def parse_args():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input_folder")
+    parser.add_argument("-o", "--output_folder")
+    parser.add_argument(
+        "-t", "--input_type", default="vcf", help="type of input files", action="store"
+    )
+    parser.add_argument(
+        "-g",
+        "--genome_build",
+        default="GRCh38",
+        help="Reference genome build",
+        action="store",
+    )
+    parser.add_argument(
+        "-e",
+        "--exome",
+        default=False,
+        help="Whether or not to use exome-renormalized COSMIC signatures",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-x",
+        "--exclude_file",
+        default="",
+        help="Path to file containing cosmic signatures to exclude, each one on a newline",
+        action="store",
+    )
+    args = vars(parser.parse_args())  # convert to dict
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    to_exclude: list | None = None
+    if args["exclude_file"]:
+        with open(args["exclude_file"], "r") as r:
+            to_exclude = [s.strip() for s in r.read().splitlines()]
+    Analyze.cosmic_fit(
+        args["input_folder"], args["output_folder"], input_type=args["input_type"],
+        exome = args["exome"], genome_build = args["genome_build"],
+        exclude_signature_subgroups = to_exclude
+    )
---
+++ nextflow/modules/sigprofilerassignment.nf
@@ -0,0 +1,50 @@
+process SIGPROFILERASSIGNMENT {
+    ext version: "0.1.9"
+
+    publishDir "$meta.out", mode: "copy", saveAs: params.saveFn
+    publishDir "$meta.log", mode: "copy", pattern: "*.log"
+
+    input:
+    tuple val(meta), path(vcfs)
+    val(is_exome)
+    val(exclude_file)
+    val(module_number)
+    //
+
+    output:
+    tuple val(meta), path(output), emit: data
+    path("*.log")
+    //
+
+    script:
+    output = Utils.getName(module_number, meta, "SigProfilerAssignment")
+    exome_flag = is_exome ? " --exome " : ""
+    exclude_flag = exclude_file != "" ? " --exclude_file ${exclude_file} " : ""
+    args = task.ext.args.join(" ")
+    check = file("$meta.out/$output")
+    if (check.exists()) {
+        """
+        cp -r $check .
+        ln -sr $meta.log/sigprofilerassignment.log .
+        """
+    } else {
+        """
+        export SIGPROFILERASSIGNMENT_VOLUME=${task.ext.volume}
+
+        mkdir vcfs
+
+        for v in *vcf.gz; do
+            cp \$v vcfs
+            gunzip vcfs/\$v
+        done
+
+        SigProfilerAssignmentWrapper.py -i vcfs \\
+            ${args} \\
+            -o ${output} \\
+            ${exome_flag} \\
+            ${exclude_flag}
+
+        get_nextflow_log.bash sigprofilerassignment.log
+        """
+    }
+}
