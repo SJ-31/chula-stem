d7b80f2e88143b7e95e58c51ad07f8027d6893ba
Shann Chongwattanankul <shanj3131@gmail.com>
2024-12-17 13:57:48 +0700

    feat: use reportlab

--- src/chula_stem/report.py
+++ src/chula_stem/report.py
@@ -7,6 +7,8 @@ import polars as pl
 import requests
 from gql import Client, gql
 from gql.transport.aiohttp import AIOHTTPTransport
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.platypus import Paragraph
 from requests import Session
 
 from chula_stem.callset_qc import IMPACT_MAP
@@ -469,3 +471,52 @@ def add_therapy_info(
 
 
 ## * Report formatter
+def style_cells(
+    start: tuple,
+    ncols: int = 0,
+    nrows: int = 0,
+    fontname: str = "",
+    fontsize: str = "",
+    align: str = "",
+    background: str = "",
+    valign: str = "",
+    textcolor=None,
+    underline: tuple = (),
+) -> list:
+    """
+    Coordinates for table style are given as (column, row)
+    """
+    parameter_map: dict = {
+        fontname: "FONTNAME",
+        fontsize: "FONTSIZE",
+        textcolor: "TEXTCOLOR",
+        background: "BACKGROUND",
+        align: "ALIGN",
+        valign: "VALIGN",
+        underline: "LINEBELOW",
+    }
+    if ncols and nrows:
+        end: tuple = start[0] + ncols - 1, start[1] + nrows - 1
+    else:
+        end = (-1, -1)
+
+    def style_helper(format: str, value) -> tuple:
+        if isinstance(value, tuple):
+            return (format, start, end) + value
+        else:
+            return (format, start, end, value)
+
+    styles: list = []
+    for param, name in parameter_map.items():
+        if param:
+            styles.append(style_helper(name, param))
+    return styles
+
+
+def add_pstyles(data: pl.DataFrame | list, style: ParagraphStyle) -> list:
+    if isinstance(data, pl.DataFrame):
+        return [
+            [Paragraph(str(s), style) for s in row] for row in data.iter_rows()
+        ]
+    else:
+        return [Paragraph(text, style) for text in data]
--- tests/test_report.py
+++ tests/test_report.py
@@ -45,7 +45,7 @@ for db in therapy_dbs:
 all_drug_info: pl.DataFrame = pl.concat(temp)
 with_therapeutics = df.join(
     all_drug_info, how="left", left_on="SYMBOL", right_on="gene"
-)
+).unique("SYMBOL")
 confident = (
     with_therapeutics.filter(
         (pl.col("Existing_variation").is_not_null()) & (pl.col("SOMATIC") == "1")
@@ -55,7 +55,8 @@ confident = (
 )
 others = confident.filter(~pl.col("VAR_ID").is_in(confident["VAR_ID"]))
 
-vtable_rename: dict = {
+# The table for renaming VEP columns for the report
+VTABLE_RENAME: dict = {
     "SYMBOL": "Gene",
     "VAF": "Variant Allele Frequency",
     "Alt_depth": "Variant Read Support",
@@ -65,3 +66,86 @@ vtable_rename: dict = {
     "Consequence": "Variant Effect",
     "CLIN_SIG": "ClinVar",
 }
+## Report generation
+VTABLE_COL_WIDTHS: dict = {
+    "Gene": 70,
+    "Variant Allele Frequency": 80,
+    "Variant Read Support": 50,
+    "Locus": 100,
+    "HGVS": 100,
+    "Variant Effect": 100,
+    "ClinVar": 100,
+}
+
+from reportlab.lib import colors
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.pdfgen import canvas
+from reportlab.platypus import LongTable, Paragraph, Spacer
+
+# (0, 0) is the bottom left of the page by default
+
+
+# It'll be best to wrap this in a class
+class ResultsReport:
+    def __init__(self, filename: str) -> None:
+        self.canv: canvas.Canvas = canvas.Canvas(filename)
+        pass
+
+    # def front_page(self) -> None:
+
+    def draw_variant_table(
+        self, variants: pl.DataFrame, width: int = 900
+    ) -> None:
+        res = (
+            variants.drop("Gene")
+            .rename(VTABLE_RENAME)
+            .select(VTABLE_RENAME.values())
+        )
+        to_data = [row for row in res.iter_rows()]
+
+
+# Flowables
+# - Flowable.drawOn(canvas, x, y)
+
+# Documents https://docs.reportlab.com/reportlab/userguide/ch5_platypus/#documents-and-templates
+# Create a list of flowables and pass it to Doc.build()
+import polars.selectors as cs
+
+res = (
+    confident.drop("Gene")
+    .rename(VTABLE_RENAME)
+    .select(VTABLE_RENAME.values())
+    .with_columns(
+        cs.by_dtype(pl.String)
+        .fill_null("-")
+        .str.replace_all("&", ", ", literal=True)
+    )
+)
+
+# Using Paragraphs to format text for word wrap
+column_style: ParagraphStyle = ParagraphStyle("cols", wordWrap="CJK")
+
+to_data = rp.add_pstyles(res, column_style)
+cols = rp.add_pstyles(res.columns, column_style)
+to_data.insert(0, cols)
+
+T = LongTable(to_data, colWidths=list(VTABLE_COL_WIDTHS.values()), repeatRows=0)
+ncols: int = len(res.columns)
+col_styles = rp.style_cells(
+    (0, 0),
+    ncols,
+    1,
+    textcolor=colors.red,
+    fontsize=13,
+    underline=(3, colors.black),
+    background=colors.lightgrey,
+)
+style_all = rp.style_cells((0, 1), background=colors.lightcyan)
+
+
+C = canvas.Canvas("/home/shannc/Bio_SDD/chula-stem/test.pdf")
+T.setStyle(col_styles + style_all)
+T.splitOn(C, 1000, 100)
+T.drawOn(C, 10, 10)
+C.showPage()  # Save state of the canvas page, and any further operations are done
+C.save()  # Write pdf
