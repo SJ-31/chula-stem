db31a37daf84860205a6b996070a8ebe82035158
Shann Chongwattanankul <shanj3131@gmail.com>
2024-12-18 15:07:58 +0700

    chore: update tests

--- tests/test_callset_qc.py
+++ tests/test_callset_qc.py
@@ -13,3 +13,9 @@ def test_variant_merge():
         grouping_cols=["Loc", "Ref", "Alt"],
     )
     result.write_csv("test_variant_merge1.tsv", separator="\t")
+
+
+def _test_main():
+    input = (
+        "/home/shannc/Bio_SDD/chula-stem/tests/vep/7-patient_10-VEP_small_1.tsv"
+    )
--- tests/test_report.py
+++ tests/test_report.py
@@ -1,142 +1,42 @@
 import chula_stem.report as rp
 import polars as pl
 from chula_stem.callset_qc import IMPACT_MAP
-from chula_stem.report import Civic, PanDrugs2, TherapyDB
+from chula_stem.report import VTABLE_COL_WIDTHS, Civic, PanDrugs2, TherapyDB
 
 small_path = (
     "/home/shannc/Bio_SDD/chula-stem/tests/vep/7-patient_10-VEP_small_1.tsv"
 )
-df: pl.DataFrame = pl.read_csv(
-    small_path, separator="\t", null_values=["NA", "."], infer_schema_length=None
-).with_columns(
-    pl.concat_str([pl.col("Loc"), pl.col("Feature")], separator="|").alias(
-        "VAR_ID"
-    )
-)
+sv_pat = "/home/shannc/Bio_SDD/chula-stem/tests/vep/7-patient_10-VEP_SV_1.tsv"
 
-##
-cache: str = "/home/shannc/Bio_SDD/chula-stem/tests/civic.json"
-pd_cache: str = "/home/shannc/Bio_SDD/chula-stem/tests/pandrugs2.json"
-therapy_dbs: list[TherapyDB] = [Civic(cache), PanDrugs2(pd_cache)]
-gene_list = list(df["SYMBOL"].unique())
-
-gene_list: list = gene_list[:10]  # Truncate for testing purposes
-gene_list: list = [
-    "BRAF",
-    "CEBPA",
-    "CDK6",
-    "HMGA2",
-    "CCNE1",
-    "FGFR1",
-    "BRCA1",
-    "MGMT",
-    "ALK",
-    "EGFR",
-    "KRAS",
-]
-
-## * Get therapy data
-temp: list[pl.DataFrame] = []
-for db in therapy_dbs:
-    find_info: pl.DataFrame = db.get_genes(gene_list, True)
-    if not find_info.is_empty():
-        found = find_info["gene"]
-        gene_list = list(filter(lambda x: x not in found, gene_list))
-        temp.append(find_info.select(TherapyDB.shared_cols))
-    if not gene_list:
-        break
-all_drug_info: pl.DataFrame = pl.concat(temp)
-with_therapeutics = df.join(
-    all_drug_info, how="left", left_on="SYMBOL", right_on="gene"
-).unique("SYMBOL")
-confident = (
-    with_therapeutics.filter(
-        (pl.col("Existing_variation").is_not_null()) & (pl.col("SOMATIC") == "1")
-    )
-    .with_columns(impact_score=pl.col("IMPACT").replace_strict(IMPACT_MAP))
-    .sort(pl.col("impact_score"), descending=True)
-)
-others = confident.filter(~pl.col("VAR_ID").is_in(confident["VAR_ID"]))
-
-# The table for renaming VEP columns for the report
-VTABLE_RENAME: dict = {
-    "SYMBOL": "Gene",
-    "VAF": "Variant Allele Frequency",
-    "Alt_depth": "Variant Read Support",
-    "Loc": "Locus",
-    "HGVSc": "HGVS",
-    "Existing_variation": "Database Name",
-    "Consequence": "Variant Type",
-    "CLIN_SIG": "ClinVar",
-}
-## * Report generation
-VTABLE_COL_WIDTHS: dict = {
-    "Gene": 70,
-    "Variant Allele Frequency": 60,
-    "Variant Read Support": 50,
-    "Locus": 80,
-    "HGVS": 90,
-    "Variant Type": 90,
-    "ClinVar": 100,
-}
-
+# Documents https://docs.reportlab.com/reportlab/userguide/ch5_platypus/#documents-and-templates
+# Create a list of flowables and pass it to Doc.build()
+import polars.selectors as cs
 from reportlab.lib import colors, units
-from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
 from reportlab.pdfgen import canvas
 from reportlab.platypus import LongTable, Paragraph, SimpleDocTemplate, Spacer
 
 # (0, 0) is the bottom left of the page by default
 
-
-# It'll be best to wrap this in a class
-class ResultsReport:
-    def __init__(self, filename: str) -> None:
-        self.canv: canvas.Canvas = canvas.Canvas(filename)
-        pass
-
-    # def front_page(self) -> None:
-
-    def draw_variant_table(
-        self, variants: pl.DataFrame, width: int = 900
-    ) -> None:
-        res = (
-            variants.drop("Gene")
-            .rename(VTABLE_RENAME)
-            .select(VTABLE_RENAME.values())
-        )
-        to_data = [row for row in res.iter_rows()]
-
-
 # Flowables
 # - Flowable.drawOn(canvas, x, y)
 
-# Documents https://docs.reportlab.com/reportlab/userguide/ch5_platypus/#documents-and-templates
-# Create a list of flowables and pass it to Doc.build()
-import polars.selectors as cs
-
-res = (
-    with_therapeutics.drop("Gene")
-    .rename(VTABLE_RENAME)
-    .select(VTABLE_RENAME.values())
-    .with_columns(
-        pl.col("HGVS").str.extract(r".*:(.*)$", 1),
-        pl.col("Variant Type").str.replace("_variant$", ""),
-        (pl.col("Variant Allele Frequency").cast(pl.Float64) * 100)
-        .round(3)
-        .map_elements(lambda x: f"{x}%", return_dtype=pl.String),
-    )
-    .with_columns(
-        cs.by_dtype(pl.String)
-        .fill_null("-")
-        .str.replace_all("&", ", ", literal=True)
-        .str.replace_all("_", " ", literal=True),
-    )
+R = rp.ResultsReport(
+    "dummy",
+    civic_cache="/home/shannc/Bio_SDD/chula-stem/tests/civic.json",
+    pandrugs2_cache="/home/shannc/Bio_SDD/chula-stem/tests/pandrugs2.json",
+    vep_small=small_path,
+    vep_sv=sv_pat,
 )
+res = R.data["all"]["small"]
 
 # Using Paragraphs to format text for word wrap
-column_style: ParagraphStyle = ParagraphStyle("cols")
+column_style: ParagraphStyle = ParagraphStyle("cols", fontSize=9)
+numeric_style: ParagraphStyle = ParagraphStyle("nums", fontSize=11)
+text_style: ParagraphStyle = ParagraphStyle("data", fontSize=10)
+data_styles: dict = {1: numeric_style, 2: numeric_style, None: text_style}
 
-to_data = rp.add_pstyles(res, column_style)
+to_data = rp.add_pstyles(res, data_styles)
 cols = rp.add_pstyles(res.columns, column_style)
 to_data.insert(0, cols)
 
@@ -165,4 +65,31 @@ loc: tuple = (10, 10)
 # splits = T.splitOn(pdf, 500, 100)
 elements = []
 elements.append(T)
-pdf.build(elements)
+
+
+def _header_footer(canvas, doc):
+    # Save the state of our canvas so we can draw on it
+    canvas.saveState()
+    styles = getSampleStyleSheet()
+
+    # Header
+    header = Paragraph(
+        "This is a multi-line header.  It goes on every page.   " * 5,
+        styles["Normal"],
+    )
+    w, h = header.wrap(doc.width, doc.topMargin)
+    header.drawOn(canvas, doc.leftMargin, doc.height + doc.topMargin - h)
+
+    # Footer
+    footer = Paragraph(
+        "This is a multi-line footer.  It goes on every page.   " * 5,
+        styles["Normal"],
+    )
+    w, h = footer.wrap(doc.width, doc.bottomMargin)
+    footer.drawOn(canvas, doc.leftMargin, h)
+
+    # Release the canvas
+    canvas.restoreState()
+
+
+pdf.build(elements, onFirstPage=_header_footer, onLaterPages=_header_footer)
