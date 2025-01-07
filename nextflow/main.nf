6d79da49d159ef98fc307cca72b38c19f0ec5ea0
Shann Chongwattanankul <shanj3131@gmail.com>
2025-01-07 08:33:36 +0700

    chore: modify script for making nextflow routine

--- nextflow/main.nf
+++ nextflow/main.nf
@@ -1,9 +1,12 @@
-include { whole_exome } from "./workflows/wes"
+include { whole_exome } from "./workflows/wes.nf"
+include { whole_exome_tumor_only } from "./workflows/wes_tumor_only.nf"
 
 workflow {
-    switch(params.routine) {
-        case "wes":
-            whole_exome();
-            break;
+    if (params.routine == "wes" && params.tumor_only) {
+        whole_exome_tumor_only()
+    } else if (params.routine == "wes") {
+            whole_exome()
+    } else {
+        println "No routine specified, exiting..."
     }
 }
---
+++ nextflow/make_routine.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+dir="${2}"
+
+if [[ -z "${dir}" ]]; then
+    echo "No directory to place routine specified!"
+    exit 1
+fi
+
+if [[ -e "${dir}"  ]]; then
+    echo "Routine dir exists"
+    exit 0
+fi
+
+mkdir "${dir}"
+
+echo "includeConfig \"\$projectDir/main.config.nf\"" > "${dir}/nextflow.config"
+echo "workDir = ???" >> "${dir}/nextflow.config"
+ln -sr ./nextflow.config "${dir}/main.config.nf"
+
+ln -sr ./main.nf "${dir}"
+ln -sr ./modules "${dir}"
+ln -sr ./bin "${dir}"
+ln -sr ./lib "${dir}"
+ln -sr ./subworkflows "${dir}"
+ln -sr ./config "${dir}"
+ln -sr ./input "${dir}"
+ln -sr ./workflows "${dir}"
