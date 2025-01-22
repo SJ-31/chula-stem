9ca3260b14ea2b5842b996f1d450f4ee65311992
Shann Chongwattanankul <shanj3131@gmail.com>
2025-01-22 14:07:58 +0700

    chore: add helper script for getting tag

---
+++ src/bash/tag_from_sam.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+file="${1}"
+record_type="${2}" # The two-letter header record type, beginning each header line
+# and starting with @
+tag="${3}" # The two-character string whose value we want
+samtools head "${1}" | grep "@${record_type}" | \
+    sed -E "s/.*$tag:([-A-Za-z0-9_,]+).*/\1/"
