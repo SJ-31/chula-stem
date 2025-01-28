86873effcc91fa1c39dbd4e1740f675c4ee297a6
Shann Chongwattanankul <shanj3131@gmail.com>
2025-01-28 11:02:14 +0700

    chore: update script for making pon

---
+++ src/bash/create_panel_of_normals.bash
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+while getopts "l:v:m:o:t:" f; do
+    case "$f" in
+        l) sample_vcfs=${OPTARG} ;; # Text file containing paths of in-house normal sample vcfs
+        m) minimum_samples=${OPTARG} ;; # Variants only accepted if they are found in this
+        # number of samples
+        v) other_vcfs=${OPTARG};; # Text file listing paths of external vcfs  e.g. dbSNP to be added into the PON
+        o) output=${OPTARG} ;;
+        t) threads=${OPTARG} ;;
+        *) echo "Invalid flag"; exit 1;;
+    esac
+done
+
+clean_vcf () {
+    bcftools view -G | \
+        bcftools annotate -x INFO,FORMAT,QUAL -O z -W -o "${1}" --threads "${threads}"
+}
+
+mkdir cleaned
+
+while read -r v; do
+    bcftools view "${v}" | clean_vcf cleaned/"$(basename "${v}")"
+done < "${other_vcfs}"
+
+bcftools isec -l "${sample_vcfs}" -n "+${minimum_samples}" -O z -p tmp
+bcftools merge tmp/*vcf.gz | clean_vcf cleaned/samples_merged.vcf.gz
+bcftools concat cleaned/*.vcf.gz -D -a -O z > "${output}"
+
+rm -R tmp
+rm -R cleaned
