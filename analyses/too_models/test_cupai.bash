95c7f4a8813d6a45ae2fe0339cc452d834ea2826
Shann Chongwattanankul <shanj3131@gmail.com>
2025-01-31 13:55:27 +0700

    chore: fix typo in metrics output

--- analyses/too_models/test_cupai.bash
+++ analyses/too_models/test_cupai.bash
@@ -5,7 +5,7 @@ for f in ../output/too_models/*/*entrez.csv; do
     prediction="${dir}/cupai.csv"
     report="${dir}/cupai_report.txt"
     cm="${dir}/cupai_confusion_matrix.csv"
-    metrics="${dir}/bpformer_metrics.txt"
+    metrics="${dir}/cupai_metrics.txt"
     ./predict_wrapper.bash -i "${f}" -p "${prediction}" -r "${report}" -c "${cm}" \
         -m "CUP-AI-Dx" -l "tumor_type" -e "${metrics}"
 done
