123dc871f905ea6c3acd6714432330fa0d0f59d5
Shann Chongwattanankul <shanj3131@gmail.com>
2025-02-07 14:38:49 +0700

    chore: add script for plotting kras variants

---
+++ analyses/pdac/gene_plot.R
@@ -0,0 +1,24 @@
+library(ensembldb)
+library(here)
+library(reticulate)
+library(paletteer)
+source(here::here("analyses", "pdac", "main.R"))
+
+
+db <- EnsDb(here("analyses", "data", "Homo_sapiens.GRCh38.113.sqlite"))
+seqlevelsStyle(db) <- "UCSC"
+
+vep_files <- list.files(data_path, pattern = "8-P[0-9_]+-VEP_small.tsv$", recursive = TRUE, full.names = TRUE)
+
+names <- utils$basename_no_ext(vep_files) |>
+  map_chr(\(x) str_extract(x, "8-(P[0-9_]+)-VEP_small", 1))
+
+P <- new.env()
+source(here("src", "R", "plotting.R"), local = P)
+
+P$plot_sample_variants(db, vep_files, "KRAS",
+  here(outdir, "kras_sample_plot.png"),
+  canonical_tx = NULL,
+  sample_names = names,
+  palette = "vapoRwave::vapoRwave"
+)
