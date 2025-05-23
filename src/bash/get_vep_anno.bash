c7496ad5c589f5ae24467f2fd9bfc32b308b72cf
Shann Chongwattanankul <shanj3131@gmail.com>
2025-05-23 15:24:48 +0700

    chore: add optional vcf fields with vep annotations

--- src/bash/get_vep_anno.bash
+++ src/bash/get_vep_anno.bash
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
-input="$1"
-output="$2"
-if [[ -z "$3" ]]; then
+while getopts "i:o:f:q:" f; do
+    case "$f" in
+        i) input=${OPTARG} ;;
+        o) output=${OPTARG} ;;
+        f) field=${OPTARG} ;;
+        q) querystr=${OPTARG} ;;
+        *)
+            echo "Not supported"
+            exit 1
+            ;;
+    esac
+done
+
+if [[ -z "$field" ]]; then
     field="ANN"
-else
-    field="$3"
 fi
 
 header_str=$(bcftools head "${input}" | \
@@ -28,4 +37,13 @@ else
     echo "${header_str}" > "${output}"
     bcftools query "${input}" -f "%INFO/${field}" | \
         sed 's/[|]/\t/g' >> "${output}"
+fi
+
+if [[ -n "${querystr}" ]]; then
+    mkdir temp
+    echo -e "${querystr}" | sed 's/[][%]//g' | sed 's/[/]/_/g' > temp/query.tsv
+    bcftools query "${input}" -f "${querystr}" >> temp/query.tsv
+    mv "${output}" temp/to_join.tsv
+    paste temp/query.tsv temp/to_join.tsv > "${output}"
+    rm -R temp
 fi
