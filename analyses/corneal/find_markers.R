0c2775175a02602de504f29d61d25d7b16631a10
Shann Chongwattanankul <shanj3131@gmail.com>
2025-07-02 13:58:14 +0700

    feat: add edgeR analysis

--- analyses/corneal/find_markers.R
+++ analyses/corneal/find_markers.R
@@ -8,6 +8,7 @@ source(paste0(R_SRC, "/", "sc_rnaseq.R"), local = S)
 source(paste0(R_SRC, "/", "plotting.R"), local = P)
 source(here::here("analyses", "corneal", ".env.R"), local = M)
 figdir <- here(M$outdir, "corneal_figures")
+SEED <- 432
 
 library(edgeR)
 library(tidyverse)
@@ -23,8 +24,8 @@ if (path.expand("~") == "/home/shannc") {
 ad <- reticulate::import("anndata")
 sc_pp <- reticulate::import("scanpy.pp")
 sc <- reticulate::import("scanpy")
-# %%
 CORNEAL_META <- read_csv(here("analyses", "corneal", "metadata.csv"))
+# %%
 
 ## * Helper functions
 
@@ -130,7 +131,16 @@ corneal_integrate <- function(obj) {
   obj <- normalize_to_pca(obj)
 
   obj <- FindNeighbors(obj, reduction = "pca") |>
-    FindClusters(algorithm = 1, cluster.name = "clusters_pre") |>
+    FindClusters(
+      algorithm = 1,
+      cluster.name = "louvain_pre",
+      random.seed = SEED
+    ) |>
+    FindClusters(
+      algorithm = 4,
+      cluster.name = "leiden_pre",
+      random.seed = SEED
+    ) |>
     RunUMAP(dims = 1:30, reduction = "pca", reduction.name = "umap_pre")
 
   obj <- IntegrateLayers(
@@ -138,11 +148,20 @@ corneal_integrate <- function(obj) {
     method = HarmonyIntegration
   ) |>
     FindNeighbors(reduction = "harmony") |>
-    FindClusters(algorithm = 1, cluster.name = "clusters_post") |> # 1 is Louvain
+    FindClusters(
+      algorithm = 1,
+      cluster.name = "louvain_post",
+      random.seed = SEED
+    ) |> # 1 is Louvain
+    FindClusters(
+      algorithm = 4,
+      cluster.name = "leiden_post",
+      random.seed = SEED
+    ) |> # 4 is Leiden
     RunUMAP(dims = 1:30, reduction = "harmony", reduction.name = "umap_post")
 
   obj[["RNA"]] <- JoinLayers(obj[["RNA"]])
-  Idents(obj) <- "clusters_post"
+  Idents(obj) <- "leiden_post"
   # They didn't pseudobulk for the DE analysis
   obj
 }
@@ -159,7 +178,12 @@ integrated <- U$read_existing(
 )
 integrated[[]]$seurat_clusters <- NULL
 integrated[[]]$cluster_confluence <- paste0(
-  integrated[[]]$clusters_post,
+  integrated[[]]$leiden_post,
+  "_passage_",
+  integrated[[]]$confluency_passage
+)
+integrated[[]]$donor_passage <- paste0(
+  integrated[[]]$donor,
   "_passage_",
   integrated[[]]$confluency_passage
 )
@@ -170,7 +194,7 @@ rm(corneal)
 markers_v1 <- U$read_existing(
   here(M$outdir, "corneal_markers.csv"),
   \(f) {
-    df <- FindAllMarkers(integrated, group.by = "clusters_post")
+    df <- FindAllMarkers(integrated, group.by = "leiden_post")
     write_csv(df, f)
     df
   },
@@ -213,24 +237,6 @@ if (path.expand("~") != "/home/shannc" && do_demux) {
   try_demux(corneal, c("G4", "G8", "G9"))
 }
 
-## ** DE analysis
-
-agg <- AggregateExpression(
-  integrated,
-  group.by = c("clusters_post", "geo"),
-  return.seurat = TRUE
-)
-agg[[]] <- left_join(
-  agg[[]],
-  dplyr::select(mutate(CORNEAL_META, geo = str_remove(prefix, "_.*")), -prefix),
-  by = join_by(geo)
-)
-
-dge <- DGEList(
-  counts = as.matrix(LayerData(agg, "counts")),
-  samples = agg[[]]
-)
-
 
 ## ** Plots
 
@@ -248,15 +254,23 @@ U$read_existing(here(figdir, "umap_post.png"), \(f) {
     integrated,
     file = f,
     reduction = "umap_post",
-    group.by = c("clusters_pre", "clusters_post", "donor")
+    group.by = c("leiden_pre", "leiden_post", "donor")
   )
 })
 
+U$read_existing(here(figdir, "umap_comparison.png"), \(f) {
+  dimplot_wrapper(
+    integrated,
+    file = f,
+    reduction = "umap_post",
+    group.by = c("leiden_pre", "leiden_post", "louvain_pre", "louvain_post")
+  )
+})
 
 ## *** Original markers
 
 markers <- list(
-  proliferative = c("CENPF", "PTTG1", "MKI67", "PCNA", "TOP2A"),
+  proliferative = c("CENPF", "PTTG1", "MKI67"),
   senescent = c("MT2A", "CDKN2A", "TAGLN"),
   fibrotic = c("ACTA2", "CD44", "COL6A1", "COL6A3"),
   cec_phenotype = c("COL4A3", "CDH2", "ALCAM", "SLC4A11"),
@@ -285,15 +299,10 @@ U$read_existing(here(figdir, "original_markers_cluster.png"), \(f) {
   ggsave(f, plot, width = 20, height = 15, dpi = 500)
 })
 
-# [2025-07-02 Wed]
-# WARNING: the dotplot is misleading: the representation of cluster 12
-# for genes PTTG1, CENPF being highly expressed is false
-# you confirmed this with the violin plot, average expression and FindAllMarkers results
-
 gene2class <- tibble(gene = markers, class = names(markers)) |>
   unnest(cols = c(gene))
 average_expr <- AggregateExpression(
-  integrated,
+  inegrated,
   features = unlist(markers)
 )$RNA |>
   as.data.frame() |>
@@ -305,22 +314,25 @@ U$read_existing(here(figdir, "original_markers_violin.png"), \(f) {
   plot <- VlnPlot(
     integrated,
     c(markers$proliferative, markers$senescent),
-    group.by = "clusters_post",
+    group.by = "leiden_post",
   )
   ggsave(f, plot, width = 15, height = 15, dpi = 500)
 })
 
 ## * Proliferative markers
-p_clust <- 11 # [2025-07-02 Wed] Cluster containing proliferative cells
+p_clust <- "g11" # [2025-07-02 Wed] Cluster containing proliferative cells
 
 # top markers from FindAllMarkers
 up_fam <- markers_v1 |>
-  dplyr::filter(cluster == p_clust) |>
+  dplyr::filter(cluster == 11) |>
   arrange(desc(avg_log2FC)) |>
   slice_head(n = 5) |>
   pluck("gene")
-
-VlnPlot(integrated, features = up_fam)
+down_fam <- markers_v1 |>
+  dplyr::filter(cluster == 11) |>
+  arrange(avg_log2FC) |>
+  slice_head(n = 5) |>
+  pluck("gene")
 
 
 p_conserved <- FindConservedMarkers(
@@ -329,18 +341,105 @@ p_conserved <- FindConservedMarkers(
   11,
   grouping.var = "confluency_passage"
 )
-clusters <- integrated[[]]$clusters_post
+# TODO: review these results
 
-dge <- normLibSizes(dge)
-mm <- model.matrix(~ clusters_post + confluency_passage, data = colData(dge))
-dge <- estimateDisp(dge, design = mm, robust = TRUE)
-fit <- glmQLFit(dge, mm, robust = TRUE)
-mean_val <- 1 / (length(clusters) - 1)
-mean_others <- paste0(
-  mean_val,
-  "*",
-  clusters[clusters != p_clust],
-  collapse = "+"
+# Double-check results of FindAllMarkers with edgeR pseudo-bulking
+
+with_edger <- function(f) {
+  agg <- AggregateExpression(
+    integrated,
+    group.by = c("leiden_post", "geo"),
+    return.seurat = TRUE
+  )
+  agg[[]] <- left_join(
+    agg[[]],
+    dplyr::select(
+      mutate(CORNEAL_META, geo = str_remove(prefix, "_.*")),
+      -prefix
+    ),
+    by = join_by(geo)
+  ) |>
+    mutate(donor_passage = paste0(donor, "_", confluency_passage))
+  dge <- DGEList(
+    counts = as.matrix(LayerData(agg, "counts")),
+    samples = agg[[]]
+  )
+  dge <- normLibSizes(dge)
+  mm <- model.matrix(~ leiden_post + donor_passage, data = dge$samples)
+  dge <- estimateDisp(dge, design = mm, robust = TRUE)
+  fit <- glmQLFit(dge, mm, robust = TRUE)
+
+  # Use pairwise comparisons rather than average
+  other_clusters <- colnames(mm) |>
+    keep(\(x) {
+      str_detect(x, "leiden_post") && !str_detect(x, p_clust)
+    })
+
+  contrasts <- paste0(
+    glue("leiden_post{p_clust}"),
+    "-",
+    other_clusters
+  )
+
+  test <- glmQLFTest(
+    fit,
+    contrast = makeContrasts(contrasts = contrasts, levels = mm)
+  )
+  write_csv(
+    as.data.frame(test),
+    here(M$outdir, "corneal_proliferative_edgeR_test_all.csv")
+  )
+
+  write_csv(as.data.frame(topTags(test)), f)
+  test
+}
+
+test <- read_existing(
+  here(M$outdir, "corneal_proliferative_edgeR_test.csv"),
+  with_edger,
+  read_csv
 )
-contrast_str <- paste0(clusters, "-", "(", mean_others, ")")
-test <- glmQLFTest(fit, contrast = )
+
+
+sig <- topTags(test) |>
+  as.data.frame() |>
+  rownames_to_column(var = "gene") |>
+  as_tibble() |>
+  dplyr::filter(PValue <= 0.01)
+
+upregulated <- sig |>
+  dplyr::filter(if_all(starts_with("logFC"), ~ . > 0)) %>%
+  mutate(
+    avgLFC = rowMeans(dplyr::select(., contains("logFC"))),
+    sdLFC = apply(dplyr::select(., contains("logFC")), 1, sd),
+  )
+write_csv(upregulated, here(M$outdir, "corneal_proliferative_edgeR_upreg.csv"))
+#
+# [2025-07-02 Wed] Original markers were found here, which is promising
+new_markers <- upregulated$gene[!upregulated$gene %in% markers$proliferative]
+
+U$read_existing(here(figdir, "new_markers.png"), \(x) {
+  plot <- DotPlot(
+    integrated,
+    features = list(original = markers$proliferative, putative = new_markers)
+  )
+  ggsave(x, plot, width = 12, height = 10, dpi = 500)
+})
+
+U$read_existing(here(figdir, "new_markers_confluency.png"), \(x) {
+  plot <- DotPlot(
+    integrated,
+    features = list(original = markers$proliferative, putative = new_markers),
+    group.by = "cluster_confluence"
+  )
+  ggsave(x, plot, width = 12, height = 10, dpi = 500)
+})
+
+U$read_existing(here(figdir, "new_markers_violin.png"), \(f) {
+  plot <- VlnPlot(
+    integrated,
+    upregulated$gene,
+    group.by = "leiden_post",
+  )
+  ggsave(f, plot, width = 12, height = 10, dpi = 500)
+})
