ec6c01c17a5a42476b53ac173bddc4835e6bcc1d
Shann Chongwattanankul <shanj3131@gmail.com>
2025-08-01 12:49:21 +0700

    bug: get stats for only tumor samples

--- analyses/crc_exome/select_genes.R
+++ analyses/crc_exome/select_genes.R
@@ -30,9 +30,17 @@ gene_tb <- unlist(snakemake@params$genes, use.names = FALSE) |>
   bind_rows()
 write_tsv(gene_tb, TARGET_FILE, col_names = FALSE)
 
-get_wanted_genes <- function(name, file, vep = TRUE) {
-  args <- c("view", glue("--targets-file {TARGET_FILE}"), file)
-
+get_wanted_genes <- function(name, file, is_paired = TRUE, vep = TRUE) {
+  args <- c(
+    "view",
+    glue("--targets-file {TARGET_FILE}")
+  )
+  if (is_paired) {
+    samples <- system2("bcftools", args = c("query", "-l", file), stdout = TRUE)
+    tumor_sample <- keep(samples, \(x) str_detect(x, "tumor"))[1]
+    args <- c(args, glue("-s {tumor_sample}"))
+  }
+  args <- c(args, file)
   tmp <- glue("{OUTDIR}/{name}.vcf")
 
   system2("bcftools", args, stdout = tmp)
@@ -51,17 +59,26 @@ get_wanted_genes <- function(name, file, vep = TRUE) {
 }
 
 vep_vcfs <- lapply(names(vcfs), \(x) {
-  get_wanted_genes(name = x, file = vcfs[[x]], vep = TRUE)
+  get_wanted_genes(
+    name = x,
+    file = vcfs[[x]],
+    vep = TRUE,
+    is_paired = snakemake@config$variant_calling$paired
+  )
 }) |>
   `names<-`(names(vcfs))
 
+
+to_double <- c("AF", "PS", "INFO_DP")
+
 combined_anno <- lapply(
   vep_vcfs,
-  mutate(
-    AF = as.double(AF),
-    PS = as.double(PS),
-    INFO_DP = as.double(INFO_DP)
-  )
+  \(x) {
+    cns <- colnames(x)
+    to_character <- cns[!cns %in% to_double]
+    mutate(x, across(all_of(to_double), as.double)) |>
+      mutate(across(all_of(to_character), as.character))
+  }
 ) |>
   list_rbind(names_to = "subject")
 
--- src/bash/get_vep_anno.bash
+++ src/bash/get_vep_anno.bash
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-while getopts "i:o:f:q:" f; do
+while getopts "i:o:f:q:s:" f; do
     case "$f" in
-        i) input=${OPTARG} ;;
-        o) output=${OPTARG} ;;
-        f) field=${OPTARG} ;;
-        q) querystr=${OPTARG} ;;
-        *)
-            echo "Not supported"
-            exit 1
-            ;;
+    i) input=${OPTARG} ;;
+    o) output=${OPTARG} ;;
+    f) field=${OPTARG} ;;
+    q) querystr=${OPTARG} ;;
+    s) sample=${OPTARG} ;;
+    *)
+        echo "Not supported"
+        exit 1
+        ;;
     esac
 done
 
@@ -17,33 +18,39 @@ if [[ -z "$field" ]]; then
     field="ANN"
 fi
 
-header_str=$(bcftools head "${input}" | \
-    grep "Consequence annotations from Ensembl VEP" | \
-    sed -r 's/.*Format:(.*).*\">/\1/' | \
+all_samples=$(bcftools query -l "${input}" | tr "\n" "," | sed -r 's/,$//')
+
+if [[ -z "${sample}" ]]; then
+    sample=${all_samples}
+fi
+
+header_str=$(bcftools head "${input}" |
+    grep "Consequence annotations from Ensembl VEP" |
+    sed -r 's/.*Format:(.*).*\">/\1/' |
     sed 's/[|]/\t/g')
 
 has_transcription_factors=$(echo "$header_str" | grep "TRANSCRIPTION_FACTORS")
 
 if [[ -n "${has_transcription_factors}" ]]; then
     echo "Removing TRANSCRIPTION_FACTORS..."
-    IFS=$'\t' read -r -a headers <<< "${header_str}"
+    IFS=$'\t' read -r -a headers <<<"${header_str}"
     final="${#headers[@]}"
     exclude=$((final - 1))
-    echo "${header_str}" | cut -f 1-"${exclude}" > "${output}"
-    bcftools query "${input}" -f "%INFO/${field}" | \
-        sed 's/[|]/\t/g' | \
-        cut -f 1-"${exclude}" >> "${output}"
+    echo "${header_str}" | cut -f 1-"${exclude}" >"${output}"
+    bcftools query "${input}" -f "%INFO/${field}" -s "${sample}" |
+        sed 's/[|]/\t/g' |
+        cut -f 1-"${exclude}" >>"${output}"
 else
-    echo "${header_str}" > "${output}"
-    bcftools query "${input}" -f "%INFO/${field}" | \
-        sed 's/[|]/\t/g' >> "${output}"
+    echo "${header_str}" >"${output}"
+    bcftools query "${input}" -f "%INFO/${field}" -s "${sample}" |
+        sed 's/[|]/\t/g' >>"${output}"
 fi
 
 if [[ -n "${querystr}" ]]; then
     mkdir temp
-    echo -e "${querystr}" | sed 's/[][%]//g' | sed 's/[/]/_/g' > temp/query.tsv
-    bcftools query "${input}" -f "${querystr}" >> temp/query.tsv
+    echo -e "${querystr}" | sed 's/[][%]//g' | sed 's/[/]/_/g' >temp/query.tsv
+    bcftools query "${input}" -f "${querystr}" >>temp/query.tsv
     mv "${output}" temp/to_join.tsv
-    paste temp/query.tsv temp/to_join.tsv > "${output}"
+    paste temp/query.tsv temp/to_join.tsv >"${output}"
     rm -R temp
 fi
