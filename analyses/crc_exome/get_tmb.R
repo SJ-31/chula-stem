381009816e0a45fd37b8b96cfdbe88599127cadb
Shann Chongwattanankul <shanj3131@gmail.com>
2025-08-01 13:13:51 +0700

    bug: correct output calls to tmb

--- analyses/crc_exome/get_tmb.R
+++ analyses/crc_exome/get_tmb.R
@@ -1,14 +1,21 @@
 library(tidyverse)
-
+library(glue)
+reticulate::use_condaenv(snakemake@config$conda)
 py_utils <- new.env()
 reticulate::source_python(glue("{snakemake@config$src$py}/utils.py"), py_utils)
 tmp <- snakemake@output$temp
-outfile <- snakemake@output$final
+outfile <- snakemake@output[[1]]
 
-py_utils$parse_multiqc_vep(snakemake@input[[1]], tmp)
+df <- py_utils$parse_multiqc_vep(snakemake@input[[1]]) |> as_tibble()
 
-read_tsv(tmp) |>
-  filter(key %in% ACCEPTED_CONSEQUENCE) |>
+
+accepted_consequence <- snakemake@config$accepted_consequence
+if (is.null(accepted_consequence)) {
+  accepted_consequence <- df$key
+}
+
+df |>
+  filter(key %in% accepted_consequence) |>
   mutate(
     sample = str_extract(sample, "[87]-(.*)-VEP_small"),
     key = map_chr(key, \(x) str_replace_all(str_to_title(x), "_", " "))
