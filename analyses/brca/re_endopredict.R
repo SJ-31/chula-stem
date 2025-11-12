2c9f9bd4267d516375d7139ece2d50cfc7bd6e2c
Shann Chongwattanankul <shanj3131@gmail.com>
2025-11-12 13:32:18 +0700

    chore: validate other gene lists with the endopredict dataset

--- analyses/brca/re_endopredict.R
+++ analyses/brca/re_endopredict.R
@@ -3,25 +3,43 @@ suppressMessages({
   library(tidyverse)
   library(glue)
   library(AnnotationHub)
+  library(ggplot2)
   library(Biobase)
+  library(caret)
   library(GEOquery)
   setAnnotationHubOption("CACHE", here(".cache", "AnnotationHub"))
+  source(here("src", "R", "utils.R"))
 })
-geo_cache <- here(".cache", "GEO")
-date <- format(Sys.time(), "%Y-%m-%d")
-cur_dir <- here("analyses", "brca")
-env <- yaml::read_yaml(here(cur_dir, "env.yaml"))
-outdir <- here("analyses", "output", "brca", "re_endopredict")
-dir.create(outdir)
-mpath <- env$metadata_path
+if (path.expand("~") == "/home/shannc") {
+  reticulate::use_condaenv("stem-base")
+} else {
+  reticulate::use_condaenv("nf")
+}
+set.seed(3110)
+library(reticulate)
 
-probe2gene <- local({
-  tb <- read_tsv(here(mpath, "GSE6532_LUMINAL_annot.txt")) |>
-    dplyr::select(probe, HUGO.gene.symbol)
-  setNames(tb$`HUGO.gene.symbol`, tb$probe)
+GEO_CACHE <- here(".cache", "GEO")
+DATE <- format(Sys.time(), "%Y-%m-%d")
+CUR_DIR <- here("analyses", "brca")
+ENV <- yaml::read_yaml(here(CUR_DIR, "env.yaml"))
+OUTDIR <- here("analyses", "output", "brca", "re_endopredict")
+dir.create(OUTDIR)
+MPATH <- ENV$metadata_path
+
+g.probe2gene <- local({
+  tb <- read_tsv(here(ENV$probe_mapping)) |>
+    dplyr::select(`HGNC symbol`, `AFFY HG U133A 2 probe`)
+  setNames(tb$`HGNC symbol`, tb$`AFFY HG U133A 2 probe`)
+})
+g.gene2probe <- local({
+  tb <- read_tsv(here(ENV$probe_mapping)) |>
+    group_by(`HGNC symbol`) |>
+    summarise(probe = list(`AFFY HG U133A 2 probe`))
+  # This is a one-to-many relationship
+  setNames(tb$probe, tb$`HGNC symbol`)
 })
 
-original_set <- read_tsv(here(cur_dir, "endopredict_candidates.tsv")) |>
+original_set <- read_tsv(here(CUR_DIR, "endopredict_candidates.tsv")) |>
   rename_with(\(x) str_replace_all(x, " ", "_"))
 
 # Authors already re-normalized the data from the different cohorts in the same way as theirs
@@ -42,20 +60,20 @@ efiles <- c(
   "GSE26971.tsv"
 )
 
-expr_dir <- here(env$raw_path, "GSE26971")
+expr_dir <- here(ENV$raw_path, "GSE26971")
 
 ## * Gather metadata
 meta <- lapply(names(mfiles), \(x) {
   file <- mfiles[[x]]
-  tb <- read_tsv(here(mpath, file))
-  spec <- env$datasets[[x]]
+  tb <- read_tsv(here(MPATH, file))
+  spec <- ENV$datasets[[x]]
   to_remap <- unlist(spec$meta_remap)
   if (!is.null(spec$id_col)) {
     tb <- dplyr::rename(tb, geo_accession = spec$id_col)
   }
   if (!is.null(to_remap)) tb <- dplyr::rename(tb, to_remap)
   if (x == "GSE6532-GPL96") {
-    tb <- mutate(tb, dmfs_time = dmfs_time / (365.24 / 12))
+    tb <- mutate(tb, dmfs_time = floor(dmfs_time / (365.24 / 12)))
   }
   to_replace <- spec$meta_replace
   for (col in names(to_replace)) {
@@ -72,7 +90,7 @@ meta <- lapply(names(mfiles), \(x) {
   }
   if (!"subcohort" %in% names(to_remap)) tb <- mutate(tb, subcohort = x)
   tb |>
-    mutate(tb, dmfs_time = floor(dmfs_time)) |>
+    mutate(tb, dmfs_time = dmfs_time) |>
     dplyr::select(all_of(shared_cols))
 }) |>
   bind_rows()
@@ -88,25 +106,73 @@ expr <- dplyr::select(expr, all_of(c("ID_REF", rownames(meta)))) |>
   as.matrix()
 expr <- expr[, sort(colnames(expr))]
 meta <- meta[sort(rownames(meta)), ]
-eset <- ExpressionSet(
+g.eset <- ExpressionSet(
   assayData = expr,
   phenoData = AnnotatedDataFrame(as.data.frame(meta))
 )
 
-eset <- eset[, !is.na(eset[["dmfs_status"]])]
-
+g.eset <- g.eset[, !is.na(g.eset[["dmfs_status"]])]
+train_idx <- createDataPartition(g.eset[["dmfs_status"]], p = 0.8)[[1]]
+g.train_eset <- g.eset[, train_idx]
+g.test_eset <- g.eset[, -train_idx]
 
 ## * Analyses
 
+result <- list(
+  de = list(),
+  lr = list(),
+  penalized_cox = list(),
+  endopredict_candidates = list(
+    selection = original_set$Gene_name,
+    probes = original_set$Probe_set
+  )
+)
+other_lists <- yaml::read_yaml(here(CUR_DIR, "gene_lists.yaml"))
+for (l in names(other_lists)) {
+  symbols <- other_lists[[l]]
+  probes <- discard(unlist(g.gene2probe[symbols]), is.na)
+  result[[l]] <- list(selection = symbols, probes = probes)
+}
+
+## ** Helper functions
+
+viz_routine <- function(embeddings, x, y, prefix) {
+  with_meta <- cbind(embeddings, pData(g.eset))
+  viz <- ggplot(
+    with_meta,
+    aes(
+      x = !!as.symbol(x),
+      y = !!as.symbol(y),
+      color = dmfs_time,
+      shape = factor(dmfs_status)
+    )
+  ) +
+    geom_point()
+  subcohort <- ggplot(
+    with_meta,
+    aes(x = !!as.symbol(x), y = !!as.symbol(y), color = subcohort)
+  ) +
+    geom_point() +
+    theme(axis.title.y = element_blank())
+  combined <- cowplot::plot_grid(viz, subcohort)
+  ggsave(
+    here(OUTDIR, glue("{prefix}_combined.png")),
+    combined,
+    width = 15,
+    height = 8
+  )
+}
+
 random_feature_performance <- function(
-    x,
-    y,
-    feature_set,
-    set_size,
-    method,
-    n = 5,
-    trControl = NULL,
-    metric = "Accuracy") {
+  x,
+  y,
+  feature_set,
+  set_size,
+  method,
+  n = 5,
+  trControl = NULL,
+  metric = "Accuracy"
+) {
   tr <- if (is.null(trControl)) {
     trainControl(method = "cv", number = 5)
   } else {
@@ -123,54 +189,219 @@ random_feature_performance <- function(
   )
 }
 
-results <- list(de = list(), lr = list(), cox = list())
+cv_result2tb <- function(cv_result) {
+  tibble(
+    lambda = cv_result$lambda,
+    mean_cv_error = cv_result$cvm,
+    stderr = cv_result$cvsd,
+  )
+}
+
+surv2structured_array <- function(surv, obj_name) {
+  py_run_string("import numpy as np")
+  as_mat <- as.data.frame(as.matrix(surv)) |>
+    mutate(status = as.logical(status)) |>
+    relocate(status, .before = time)
+  py$tmp <- as_mat
+  py_run_string(
+    glue(
+      "{obj_name} = np.array([(i['status'], i['time']) for _, i in tmp.iterrows()],
+ dtype = [('s', 'bool'), ('t', 'float32')])"
+    )
+  )
+}
+
+
+## ** Visualization
+
+## *** PCA
+pca_obj <- read_existing(
+  here(OUTDIR, "prcomp.rds"),
+  \(f) {
+    obj <- prcomp(t(exprs(g.eset)))
+    saveRDS(obj, f)
+    obj
+  },
+  readRDS
+)
+
+viz_routine(as.data.frame(pca_obj$x), "PC1", "PC2", "pca")
+
+
+## *** UMAP
+
+umap_obj <- read_existing(
+  here(OUTDIR, "umap.rds"),
+  \(f) {
+    module <- reticulate::import("umap")
+    umap <- module$UMAP()
+    vals <- umap$fit_transform(t(exprs(g.eset))) |>
+      `colnames<-`(c("U1", "U2")) |>
+      as_tibble()
+    saveRDS(vals, f)
+    vals
+  },
+  readRDS
+)
+
+viz_routine(umap_obj, "U1", "U2", "umap")
 
 ## ** On binary outcome of no metastasis in the observed period
+
+# NOTE: This and logistic regression shouldn't do so well
 
 ## *** DE
 
 library(limma)
 mm <- cbind(
-  as.integer(eset[["dmfs_status"]] == 0),
-  as.integer(eset[["dmfs_status"]] == 1)
+  as.integer(g.train_eset[["dmfs_status"]] == 0),
+  as.integer(g.train_eset[["dmfs_status"]] == 1)
 ) |>
   as.data.frame()
 colnames(mm) <- c("no_recurrence", "recurrence")
 
-fit <- lmFit(eset, mm)
+fit <- lmFit(g.train_eset, mm)
 contrast <- makeContrasts(no_recurrence - recurrence, levels = mm)
 fit2 <- contrasts.fit(fit, contrast)
 fit2 <- eBayes(fit2, robust = TRUE)
-top <- topTable(fit2, number = dim(eset)[1], p.value = 0.05)
-top$symbol <- probe2gene[rownames(top)]
+top <- topTable(fit2, number = dim(g.train_eset)[1], p.value = 0.05)
+top$symbol <- g.probe2gene[rownames(top)]
 
-results$de$selection <- unique(top$symbol) |> discard(is.na)
-results$de$common <- intersect(original_set$Gene_name, top$symbol)
-
-## **** Check selection
-
-de_filtered <- eset[rownames(expr) %in% rownames(top), ]
-library(caret)
-
-xgb_mod <- train(
-  x = t(exprs(de_filtered)),
-  y = factor(eset[["dmfs_status"]]),
-  method = "xgbTree",
-  trControl = trainControl(method = "cv", number = 5)
-)
-
-de_random_accs <- random_feature_performance(
-  x = t(exprs(eset)),
-  y = factor(cur[["dmfs_status"]]),
-  feature_set = rownames(eset),
-  method = "xgbTree",
-  set_size = nrow(top),
-)
-# [2025-11-10 Mon] Random sets have essentially indistinguishable performance from the DE sets
+result$de$selection <- unique(top$symbol) |> discard(is.na)
+result$de$probes <- rownames(top)
+result$de$common <- intersect(original_set$Gene_name, top$symbol)
 
 ## *** Logistic regression with Lasso
 
 ## ** Survival analysis
 
-# [2025-11-10 Mon] TODO: read up on survival analysis and how to cross-validate
-# you wanna see whether random gene selections will do just as well as the traditional selection process
+library(glmnet)
+library(survival)
+
+g.response <- Surv(
+  time = g.train_eset[["dmfs_time"]],
+  event = g.train_eset[["dmfs_status"]]
+)
+g.test_time <- g.train_eset[["dmfs_time"]]
+g.test_status <- g.train_eset[["dmfs_status"]]
+g.test_response <- Surv(
+  time = g.test_eset[["dmfs_time"]],
+  event = g.test_eset[["dmfs_status"]]
+)
+
+
+## *** Multivariable Cox
+
+glmnet_cox_wrapper <- function(name, eset, penalized = FALSE) {
+  read_existing(
+    here(OUTDIR, glue("{name}.rds")),
+    \(f) {
+      x <- t(exprs(eset))
+      y <- g.response
+      if (penalized) {
+        mod <- cv.glmnet(x, y, family = "cox", type.measure = "C", keep = TRUE)
+      } else {
+        mod <- bigGlm(x, y, family = "cox")
+      }
+      saveRDS(mod, f)
+      mod
+    },
+    readRDS
+  )
+}
+
+penalized_cox <- glmnet_cox_wrapper("penalized_cox", g.train_eset, TRUE)
+
+
+coefs <- coef(penalized_cox, s = "lambda.min") |>
+  as.matrix() |>
+  `colnames<-`("beta") |>
+  as.data.frame() |>
+  rownames_to_column(var = "ID_REF") |>
+  mutate(symbol = g.probe2gene[ID_REF])
+
+
+nonzero <- dplyr::filter(coefs, beta > 0)
+
+result$penalized_cox$selection <- unique(nonzero$symbol)
+result$penalized_cox$probes <- nonzero$ID_REF
+result$penalized_cox$common <- intersect(
+  original_set$Gene_name,
+  result$cox$selection
+)
+
+## *** Univariate Cox
+
+# This is what the original authors did
+
+# TODO:
+
+## * Feature set comparison
+
+# Compare the best C measure that other feature sets can obtain against the one
+# obtained by the full penalized cox model
+
+# - Will include...
+# the original paper's selection
+# %%
+
+# BUG: something's wrong with predict.glmnet
+# it doesn't work on the models trained with the feature subset but you have no idea why
+
+# Will resort to using scikit-survival. [2025-11-11 Tue] finish writing the below
+
+assess_helper <- function(model) {
+  assess.glmnet(
+    model,
+    newx = t(exprs(g.test_eset)),
+    newy = Surv(
+      time = g.test_eset[["dmfs_time"]],
+      event = g.test_eset[["dmfs_status"]]
+    ),
+    family = "cox"
+  ) |>
+    as.data.frame()
+}
+
+
+np <- import("numpy")
+sslm <- import("sksurv.linear_model")
+ssm <- import("sksurv.metrics")
+
+surv2structured_array(g.response, "y_train")
+
+evaluate_cox <- function(feature_subset = NULL, name = NULL) {
+  cur_eset <- g.train_eset[rownames(g.train_eset) %in% feature_subset, ]
+  coxnet <- sslm$CoxnetSurvivalAnalysis()
+  coxnet$fit(t(exprs(cur_eset)), py$y_train)
+  cic_result <- ssm$concordance_index_censored(
+    g.test_status,
+    g.test_time,
+    coxnet$predict(t(exprs(g.train_eset)))
+  )
+  result <- list()
+  result$cindex <- cic_result[1]
+  result$concordant <- cic_result[2]
+  result$discordant <- cic_result[3]
+  result$tied_risk <- cic_result[4]
+  result$tied_time <- cic_result[5]
+  result$name <- name
+  return(as.data.frame(result))
+}
+
+c_indices <- lapply(names(result), \(method) {
+  if (length(result[[method]]) > 0) {
+    df <- evaluate_cox(
+      feature_subset = result[[method]]$probes,
+      name = method
+    )
+  }
+  ## mutate(df, method = method)
+})
+
+# TODO: [2025-11-11 Tue] the below work, but probably should make a helper function for this
+
+## |>
+##   bind_rows()
+
+# Also wanna see whether random gene selections will do just as well as the traditional selection process
