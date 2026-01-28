2df04e08e843a89b2a07b44837c5e43d7d1c00b3
Shann Chongwattanankul <shanj3131@gmail.com>
2026-01-28 12:36:16 +0700

    chore: update vaf config

--- analyses/crc_exome/make_vaf_plot.sh
+++ analyses/crc_exome/make_vaf_plot.sh
@@ -2,4 +2,4 @@
 
 config=$(realpath ./vaf_config.yaml)
 cd ../../snakemake/wes_vaf_plot/ || exit
-snakemake -c 1 --configfile "${config}"
+snakemake -c 1 --configfile "${config}" --keep-incomplete --rerun-triggers mtime
