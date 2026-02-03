6738ddb1390630ff450507e405f57f3fb1cd3a75
Shann Chongwattanankul <shanj3131@gmail.com>
2026-02-03 11:17:06 +0700

    chore: add option to get primers for c gene

--- snakemake/tcr/scripts/primers.py
+++ snakemake/tcr/scripts/primers.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env ipython
 
 import json
+import re
+import subprocess as sp
 from functools import partial
 from pathlib import Path
 from typing import Callable, Literal
 
 import anndata as ad
 import mudata as md
+import numpy as np
 import polars as pl
 import polars.selectors as cs
 import pydna.tm as tm
+import pytest
 import scirpy as ir
+from Bio import SeqIO
+from loguru import logger
 from pydna.design import Amplicon, pcr, primer_design
 from pydna.dseqrecord import Dseqrecord
 from pydna.primer import Primer
+from pyhere import here
+from skbio.alignment import pair_align
+from skbio.sequence import DNA
 
 try:
     from snakemake.script import snakemake as smk
@@ -30,6 +39,7 @@ WANTED_COLS = [
     "v_sequence_start",
     "v_call",
     "j_call",
+    "c_call",
     "d_call",
     "v_sequence_end",
     "sequence",
@@ -91,6 +101,103 @@ def calc_tm(
         return tm.tm_neb(seq, prodcode=prodcode, **kws)
 
 
+def get_chain_name_from_call(call: str) -> Literal["TRB", "TRA", "TRD", "TRG"]:
+    "Calls are assumed to follow IMGT nomenclature"
+    # https://imgt.org/IMGTScientificChart/Nomenclature/IMGTallelepolymorphism.html
+    if re.match("^TRB", call):
+        return "TRB"
+    if re.match("^TRG", call):
+        return "TRG"
+    if re.match("^TRD", call) or re.match(".*TRA.*DV.*", call):
+        return "TRD"
+    if re.match("^TRA", call):
+        return "TRA"
+    raise ValueError("Chain name couldn't be determined  from call")
+
+
+def get_imgt_seqs(
+    file: Path, gene: Literal["c", "d", "j", "v"], species: str = "Homo sapiens"
+) -> list[DNA]:
+    """Return fasta entries corresponding to `gene` in a fasta file with IMGT headers"""
+    seqs = []
+    region = f"{gene.upper()}-REGION"
+    for record in SeqIO.parse(file, "fasta"):
+        desc = record.description.split("|")
+        is_constant = desc[-1] == "~CONSTANT"
+        if is_constant and gene == "c" or (desc[4] == region):
+            seq = str(record.seq)
+            if seq.islower():
+                seq = seq.upper()
+            seqs.append(
+                DNA(
+                    seq,
+                    metadata={"id": desc[0], "description": "|".join(desc)},
+                )
+            )
+    return seqs
+
+
+def get_best_alignment(query: DNA, references: list[DNA]) -> tuple[float, DNA]:
+    ends = (
+        [True, False] if len(query) <= np.mean([len(s) for s in references]) else None
+    )
+    cur_best = (pair_align(query, references[0], free_ends=ends).score, references[0])
+    for s in references[1:]:
+        alignment = pair_align(query, s, free_ends=ends)
+        if alignment.score > cur_best[0]:
+            cur_best = (alignment.score, s)
+    return cur_best
+
+
+def get_c_sequence_from_call(
+    airr_data: dict,
+    chain: str,
+    try_match_allele: bool = True,
+) -> str:
+    """
+    Return the sequence of an AIR gene from its call (predicted identity)
+
+    Parameters
+    ----------
+    try_match_allele : boolean
+        If the call is generic and does not match an allele, then return the first allele
+        for the gene sequence unless `try_match_allele` is True.
+        If it is, the sequence of the allele with the highest alignment score is returned
+    """
+    call = airr_data[f"{chain}_c_call"]
+    data_dir: Path = Path(
+        sp.run("stitchr -dd", shell=True, capture_output=True).stdout.decode().strip()
+    )
+    chain_name: str = get_chain_name_from_call(call)
+    seq_file = data_dir / "HUMAN" / f"{chain_name}.fasta"
+    if not seq_file.exists():
+        raise ValueError(f"{chain_name}.fasta not found in stitchr data directory")
+    candidates = get_imgt_seqs(seq_file, "c")
+    if not candidates:
+        raise ValueError(
+            f"No sequences for C gene could be found in {seq_file}. Try checking its contents"
+        )
+    if not try_match_allele or len(candidates) == 1:
+        return str(candidates[0])
+    full = airr_data[f"{chain}_sequence"]
+    j_end = airr_data[f"{chain}_j_sequence_end"]
+    seq = full[j_end + 1 :]
+    score, best_seq = get_best_alignment(DNA(seq), candidates)
+    return str(best_seq)
+
+
+def add_new_c(airr_data: dict, chain: str, c_seq: str) -> str:
+    """
+    Return full-length AIR sequence modified to have a new C gene sequence
+
+    This function simply strips any sequence downstream of airr_data["j_sequence_end"]
+    and replaces it with `c_seq`
+    """
+    j_end = airr_data[f"{chain}_j_sequence_end"]
+    cut = airr_data[f"{chain}_sequence"][:j_end]
+    return cut + c_seq
+
+
 # TODO: add in support for adding arbitrary overhang sequences
 def get_cdr3_fusion_primers(
     airr_data: dict,
@@ -98,22 +205,39 @@ def get_cdr3_fusion_primers(
     how: Literal["pydna", "manual"] = "pydna",
     forward_len: int = 30,
     reverse_len: int = 30,
+    end_gene: Literal["c", "j"] = "c",
     tm_func: Callable[[str], float] = tm.tm_default,
     target_tm: float = 55.0,
 ) -> dict:
     full: Dseqrecord = Dseqrecord(airr_data[f"{chain}_sequence"])
+    end_gene = end_gene.lower()
     p1_start, p1_end = (
         airr_data[f"{chain}_v_sequence_start"],
         airr_data[f"{chain}_cdr3_end"],
     )
+    end_lookup: str = f"{chain}_{end_gene}_sequence_end"
+    full_seq: str = airr_data[f"{chain}_sequence"]
+    if end_lookup not in airr_data and end_gene == "c":
+        logger.info("C gene sequence missing, retrieving from call...")
+        new_c_seq: str = get_c_sequence_from_call(
+            airr_data, chain=chain, try_match_allele=True
+        )
+        full_seq = add_new_c(airr_data, chain, new_c_seq)
+        airr_data[f"{chain}_{end_gene}_sequence_start"] = (
+            airr_data[f"{chain}_j_sequence_start"] + 1
+        )
+        airr_data[f"{chain}_{end_gene}_sequence_end"] = len(full_seq)
+    elif end_lookup not in airr_data:
+        # TODO: you could also do this for the j call...
+        raise ValueError("J gene sequence end not present in AIR data")
     p2_start, p2_end = (
         airr_data[f"{chain}_cdr3_start"],
-        airr_data[f"{chain}_j_sequence_end"],
+        airr_data[f"{chain}_{end_gene}_sequence_end"],
     )
     results = {}
     for i, (start, end) in enumerate([(p1_start, p1_end), (p2_start, p2_end)]):
         if how == "pydna":
-            target = Dseqrecord(airr_data[f"{chain}_sequence"][start:end])
+            target = Dseqrecord(full_seq[start:end])
             amp = primer_design(target, target_tm=target_tm, tm_func=tm_func)
         elif how == "manual":
             fwd = full[start : start + forward_len]
@@ -212,6 +336,7 @@ def format_one_sample(
             how=cfg.get("how", "pydna"),
             forward_len=cfg.get("forward_len", 30),
             reverse_len=cfg.get("reverse_len", 30),
+            end_gene=cfg.get("end_gene", "c"),
         )
         fmt: pl.DataFrame = PrimerFmt("df")(primers)
         exprs = []
@@ -299,3 +424,31 @@ def create_primers():
 
 if fn := globals().get(smk.rule):
     fn()
+
+# * Tests
+
+
+@pytest.fixture
+def airr_data_test():
+    combined = md.read_h5mu(here("tests", "data", "airr_test.h5mu"))
+    return combined["airr"]
+
+
+def test_get_c(airr_data_test):
+    chain = "VJ_1"
+    df = pl.from_pandas(
+        ir.get.airr(airr_data_test, WANTED_COLS, chain=chain),
+        include_index=True,
+    )
+    dct = next(df.iter_rows(named=True))
+    full = dct[f"{chain}_sequence"]
+    j_end = f"{chain}_j_sequence_end"
+    j_start = f"{chain}_j_sequence_start"
+    old_j = full[dct[j_start] : dct[j_end]]
+    old_c = full[dct[j_end] :]
+    new_c = get_c_sequence_from_call(dct, chain, try_match_allele=True)
+    print(f"Old: {old_c}\nNew: {new_c}")
+    print(f"Old full: {full}")
+    new_full = add_new_c(dct, chain, new_c)
+    print(f"New full: {new_full}")
+    assert old_j == new_full[dct[j_start] : dct[j_end]]
