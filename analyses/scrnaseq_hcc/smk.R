15c2ec6290be23e423f6e8f2b5227ce2ece38bef
Shann Chongwattanankul <shanj3131@gmail.com>
2026-02-19 13:25:21 +0700

    chore: add rule for clustering samples with TMM normalization and consensus clustering

--- analyses/scrnaseq_hcc/smk.R
+++ analyses/scrnaseq_hcc/smk.R
@@ -1,8 +1,6 @@
 suppressMessages({
   library(reticulate)
   library(tidyverse)
-  library(zellkonverter)
-  library(SingleCellExperiment)
   if (exists("snakemake")) {
     use_condaenv(snakemake@config$conda)
   } else {
@@ -18,7 +16,11 @@ if ("rlang" %in% utils::installed.packages()[, 1]) {
   options(error = rlang::entrace, rlang_backtrace_on_error = "full")
 }
 
-## * DE analysis
+if (exists("snakemake")) {
+  RCONFIG <- snakemake@config[[snakemake@rule]]
+} else {
+  RCONFIG <- list()
+}
 
 ## * Cluster-fold similarity
 
@@ -65,6 +67,8 @@ cfs_setup <- function(adata, subset_hvgs = FALSE, ...) {
 
 do_cfs <- function(adata = NULL, kws = NULL, setup_kws = NULL) {
   library(ClusterFoldSimilarity)
+  library(SingleCellExperiment)
+  library(zellkonverter)
 
   if (is.null(adata)) {
     adata <- ad$read_h5ad(snakemake@input[[1]])
@@ -83,10 +87,12 @@ do_cfs <- function(adata = NULL, kws = NULL, setup_kws = NULL) {
     }
   ) |>
     `names<-`(samples)
-  sim_table <- do.call(
-    \(...) clusterFoldSimilarity(sces, sampleNames = names(sces)),
-    kws
-  )
+  sim_table <- suppressMessages({
+    do.call(
+      \(...) clusterFoldSimilarity(sces, sampleNames = names(sces)),
+      kws
+    )
+  })
   communities <- findCommunitiesSimmilarity(sim_table)
 
   if (exists("snakemake")) {
@@ -99,6 +105,62 @@ do_cfs <- function(adata = NULL, kws = NULL, setup_kws = NULL) {
   list(similarities = sim_table, communities = communities)
 }
 
+## * Clustering
+
+cluster_samples <- function() {
+  library(ConsensusClusterPlus)
+
+  adata <- ad$read_h5ad(snakemake@input[[1]])
+  features <- read_lines(snakemake@input[[2]])
+  adata <- adata[, rownames(adata$var) %in% features]
+  prefix <- snakemake@params$prefix
+
+  kws <- RCONFIG$kws
+
+  bulked <- dc$pp$pseudobulk(tmp, "sample", groups_col = NULL)
+  gene_mask <- sc$pp$filter_genes(bulked, min_counts = 50, inplace = FALSE)
+  cell_mask <- sc$pp$filter_cells(bulked, min_counts = 50, inplace = FALSE)
+  bulked <- bulked[cell_mask[[1]], gene_mask[[1]]]
+
+  dge <- edgeR::DGEList(t(bulked$X))
+  rownames(dge) <- rownames(bulked$var)
+  colnames(dge) <- rownames(bulked$obs)
+  dge <- edgeR::normLibSizes(dge, method = "TMM")
+  expr <- edgeR::cpm(dge, log = TRUE)
+
+  result <- do.call(\(...) ConsensusClusterPlus(d = expr, ...), kws)
+  consensus <- calcICL(result)
+
+  plot <- make_consensus_plot(
+    consensus$itemConsensus,
+    RCONFIG$consensus_palette
+  )
+  tb <- lapply(seq(2, length(result)), \(k) {
+    clst <- result[[k]]$consensusClass
+    tibble(sample = names(clst), !!as.symbol(k) := paste0(prefix, "_", clst))
+  }) |>
+    purrr::reduce(\(x, y) inner_join(x, y, by = join_by(sample)))
+
+  write_csv(tb, snakemake@output[[1]])
+  ggsave(plot, snakemake@output[[2]])
+}
+
+make_consensus_plot <- function(obj, palette = NULL) {
+  plot <- ggplot(
+    consensus$itemConsensus,
+    aes(x = item, y = itemConsensus, fill = factor(cluster))
+  ) +
+    geom_bar(position = "fill", stat = "identity") +
+    theme(axis.text.x = element_text(angle = 90)) +
+    xlab("Sample") +
+    ylab("Consensus score") +
+    guides(fill = guide_legend(title = "Cluster assignment")) +
+    facet_wrap(~k)
+  if (!is.null(palette)) {
+    plot <- plot + scale_fill_paletteer_d(palette)
+  }
+  plot
+}
 
 ## * Entry
 
