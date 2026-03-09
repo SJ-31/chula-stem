9f17fd78895c0af0dd8745c4c81b6570b65a190d
Shann Chongwattanankul <shanj3131@gmail.com>
2026-03-09 10:07:43 +0700

    chore: update tests

--- tests/session.py
+++ tests/session.py
@@ -1,42 +1,135 @@
 #!/usr/bin/env ipython
 
+from importlib import util
+from pathlib import Path
+from typing import Callable, Literal
 
+import anndata as ad
+import mudata as md
 import numpy as np
-import pandas as pd
+import polars as pl
+import polars.selectors as cs
+import pydna.tm as tm
+import scirpy as ir
+from great_tables import GT
+from loguru import logger
+from pydna.design import Amplicon, pcr, primer_design
+from pydna.dseqrecord import Dseqrecord
+from pydna.primer import Primer
+from pyhere import here
+from skbio import DNA
+from skbio.alignment import PairAlignPath, pair_align
 
-file = (
-    "/home/shannc/Bio_SDD/chula-stem/analyses/output/HCC_abundance/hcc_expr_entrez.csv"
+
+def load_file_as_module(name, location):
+    spec = util.spec_from_file_location(name, location)
+    module = util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+
+mod = load_file_as_module("primers", here("snakemake", "tcr", "scripts", "primers.py"))
+
+# %%
+
+combined = md.read_h5mu(
+    here("analyses", "output", "pdac_tcr", "2026-01-22", "combined.h5mu")
 )
 
 
-# * CUP-AI-Dx
-# <2025-01-29 Wed> Finished the main function for prediction,
-# just need to test out the classification performance
-data_dir = "/home/shannc/Bio_SDD/chula-stem/extras/CUP-AI-Dx/data"
+# NOTE: bd rhapsody data doesn't seem to give the indices for cdr3/junction, so
+# we will get this manually by alignment
+def locate_cdr3(
+    cdr3: str,
+    full: str,
+    j_end: int | None = None,
+    v_start: int | None = None,
+    exact_match: bool = True,
+) -> tuple[int, int]:
+    """Helper function to locate CDR3 within full-length AIR sequence
+    TODO: you need this to actually use the biology. It will fail if there
+    is a subsequence of v gene that happens to be identical to cdr3
 
-target_genes = pd.read_csv(
-    f"{data_dir}/features_791.csv", header=None, index_col=0
-).index
 
-df = pd.read_csv(file, index_col=0)
-if "X" not in str(df.index[0]):
-    df.index = [f"X{i}" for i in df.index]
+    Parameters
+    ----------
+    exact_match : bool
 
-rows = []
-for t in target_genes:
+    Notes
+    -----
+    We expect that the cdr3
+
+    """
     try:
-        rows.append(df.loc[[t],])
-    except KeyError:
-        rows.append(pd.DataFrame(0, columns=df.columns, index=[t]))
-selected = pd.concat(rows, axis=0)
+        cdr3_start: int = full.index(cdr3)
+        cdr3_end = cdr3_start + len(cdr3)
+    except ValueError:
+        if exact_match:
+            raise ValueError("Exact match for cdr3 sequence in `full` not found")
+        logger.warning("Exact match for cdr3 not found. Resorting to alignment")
+        res = pair_align(cdr3, full, mode="global", free_ends=(True, False))
+        path: PairAlignPath = res.paths[0]
+        # "global" is guaranteed to have at least one path
+        coords: np.ndarray = path.to_coordinates()
+        cdr3_start, cdr3_end = coords[1].tolist()[1:3]
+    # if v_start and cdr3_start <= v_start:
+    #     logger.info(v_start, cdr3_start)
+    #     raise ValueError("cdr3 sequence start invalid: before start of V gene")
+    # if j_end and cdr3_end >= j_end:
+    #     raise ValueError("cdr3 sequence end invalid: after end of J gene")
+    return cdr3_start, cdr3_end
 
 
-samples = list(df.columns)
-transposed = np.transpose(df)
+wanted_clones = [["136", "PDAC83"], ["354", "PDAC82"]]
+df = mod.filter_wanted_clones(
+    combined["airr"],
+    {"wanted_clones": wanted_clones},
+    extras=["fwr1", "fwr2", "fwr3", "fwr4", "cdr2", "cdr1"],
+)
 
-# * BPformer
-sample_file = "/home/shannc/Bio_SDD/chula-stem/extras/BPformer/RNAseq/Raw/GEO-RNA-m.pkl"
-sample = pd.read_pickle(sample_file)
+# * Testzone
+# [2026-01-29 Thu] TODO: All you want is something that can index the cdr3...
+ww = next(df.iter_rows(named=True))
+cdr3 = ww["VJ_1_cdr3"]
+lcdr3 = len(cdr3)
+v_start = ww["VJ_1_v_sequence_start"]
+v_end = ww["VJ_1_v_sequence_end"]
+cdr3_start = ww["VJ_1_cdr3_start"]
+cdr3_end = ww["VJ_1_cdr3_end"]
+v_end = ww["VJ_1_v_sequence_end"]
+j_end = ww["VJ_1_j_sequence_end"]
+full = ww["VJ_1_sequence"]
+v_seq = full[v_start:v_end]
+up_to = cdr3_end - v_start
 
-sample2_file = "/home/shannc/Bio_SDD/chula-stem/extras/BPformer/data/TCGA-RNA-p-m.pkl"
-s2 = pd.read_pickle(sample2_file)
+
+forward = full[v_start : v_start + 30]
+reverse = str(DNA(full[cdr3_start:cdr3_end]).reverse_complement())
+
+
+cur, dct = mod.format_one_sample(ww, cfg={"end_gene": "j"})
+
+
+res = mod.assemble_one_construct(
+    ww,
+    ["VJ_1", "VDJ_1"],
+    {
+        "include_leader": True,
+        "include_c": True,
+        "sequences": {
+            "linker": "TTA",
+            "flanking": {"five_prime": "TACGCCGCAT", "three_prime": "CGCGAGCTA"},
+            "c_gene": {
+                "TRB": "/home/shannc/Bio_SDD/chula-stem/analyses/pdac_tcr/construct_sequences/murinized_trbc.fasta",
+                "TRA": "/home/shannc/Bio_SDD/chula-stem/analyses/pdac_tcr/construct_sequences/murinized_trac.fasta",
+            },
+        },
+    },
+)
+names = res["named"]
+validation = res["validation"]
+ax, _ = res["plot"].plot(figure_width=5)
+ax.figure.show()
+cat = DNA.concat(names.values())
+
+# BUG: resolve C gene stop codon issue
--- tests/test_pdac_subtyping.py
+++ tests/test_pdac_subtyping.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env ipython
+import re
 from pathlib import Path
 
 import anndata as ad
@@ -6,14 +7,20 @@ import chula_stem.pdac_subtyping as pds
 import h5py
 import numpy as np
 import pandas as pd
+import plotnine as gg
 import scanpy as sc
-from chula_stem.r_utils import tximport
+from chula_stem.plotting import plot_classifier_curve
+from chula_stem.r_utils import read_geo, tximport
 from chula_stem.sc_rnaseq import annotate_adata_vars
 from pyhere import here
 from scipy import sparse
+from sklearn.metrics import RocCurveDisplay, roc_curve
+
+# %%
 
 tx2gene = pd.read_csv(here("analyses", "data", "tx2gene.tsv"), sep="\t")
 workdir = here("analyses", "pdac_subtyping")
+pdata = here("analyses", "data_all", "public_data")
 
 manifest = pd.read_csv(here(workdir, "manifest.csv"))
 
@@ -33,17 +40,48 @@ if not adata_path.exists():
 else:
     adata = ad.read_h5ad(adata_path)
 
-pds.moffitt_score(adata)
-adata.obs.loc[:, "moffitt_class"] = adata.obs["moffitt_pr_basal"].apply(
-    lambda x: "basal-like" if x > 0.5 else "other"
-)
+model = pds.MoffittBasal(threshold=0.44)  # From running last time
+model.fit(adata=adata)
+model.predict(inplace=True)
 
+adata.layers["log_abundance"] = adata.layers["abundance"].copy()
+sc.pp.log1p(adata, layer="log_abundance")
 
 has_expr = adata[adata.obs["rnaseq_available"], :]
-
 sc.pl.heatmap(
     has_expr,
-    var_names=["GATA6", "SMAD4", "CDKN2A"],
-    groupby="moffitt_class",
-    layer="abundance",
+    var_names=["GATA6", "SMAD4", "CDKN2A", "TP53"],
+    groupby="moffitt_is_basal",
+    layer="log_abundance",
+    swap_axes=True,
 )
+
+
+def test_with_moffitt_original():
+    moffitt_data = here(pdata, "GEO", "GSE71729", "GSE71729_series_matrix.txt.gz")
+    moffitt = read_geo(moffitt_data)
+    model = pds.MoffittBasal()
+    moffitt.obs.rename(
+        {"tumor_subtype_0na_1classical_2basal:ch2": "subtype"}, inplace=True, axis=1
+    )
+    moffitt.obs.loc[:, "is_basal-like"] = moffitt.obs["subtype"].replace(
+        {"2": 1, "1": 0, "0": np.nan}
+    )
+    moffitt = moffitt[~moffitt.obs["is_basal-like"].isna(), :]
+    model.fit(moffitt)
+    model.tune(y_true="is_basal-like")
+    print(f"New threshold: {model.threshold}")
+
+    model.predict(inplace=True)
+    bplot = (
+        gg.ggplot(moffitt.obs, gg.aes(x="is_basal-like", y="moffitt_pr_basal"))
+        + gg.geom_boxplot()
+    )
+
+    plot = plot_classifier_curve(
+        curve="roc",
+        y_true=moffitt.obs["is_basal-like"],
+        y_score=moffitt.obs["moffitt_pr_basal"],
+    )
+    plot.show()
+    # [2026-03-06 Fri] From running this, you got the best threshold to be 0.44
