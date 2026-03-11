f56205237c2ac3d43d872f58c5acf6ac2417cbd4
Shann Chongwattanankul <shanj3131@gmail.com>
2026-03-11 09:09:57 +0700

    chore: remove unused flags in script

--- nextflow/bin/standardize_vcf_clean.bash
+++ nextflow/bin/standardize_vcf_clean.bash
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 # Helper script to clean up and prepare vcf file for standardization
-while getopts "a:i:c:r:t:n:v:o:f:" f; do
+while getopts "i:c:r:o:" f; do
     case "$f" in
-        r) reference=${OPTARG} ;; # Reference genome fasta
-        c) clear_flag=${OPTARG} ;;
-        i) input=${OPTARG} ;;
-        o) output=${OPTARG} ;;
-        *)
-            echo "Invalid flag!"
-            exit 1
-            ;;
+    r) reference=${OPTARG} ;; # Reference genome fasta
+    c) clear_flag=${OPTARG} ;;
+    i) input=${OPTARG} ;;
+    o) output=${OPTARG} ;;
+    *)
+        echo "Invalid flag!"
+        exit 1
+        ;;
     esac
 done
 
-clean_vcf () {
-    awk 'BEGIN { FS="\t"; OFS="\t" } $5 !~ /\*/ {print}' | \
-    bcftools norm --fasta-ref "${reference}" \
-        --check-ref w -O z > "${1}"
+clean_vcf() {
+    awk 'BEGIN { FS="\t"; OFS="\t" } $5 !~ /\*/ {print}' |
+        bcftools norm --fasta-ref "${reference}" \
+            --check-ref w -O z >"${1}"
 }
 
 if [[ -n "${clear_flag}" ]]; then
