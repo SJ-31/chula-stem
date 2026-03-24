cdf5ab0ef9e5e10f1eddeef701792d23eee53f30
Shann Chongwattanankul <shanj3131@gmail.com>
2026-03-24 15:30:21 +0700

    chore: finalize clustering script

--- analyses/cluster_reproducibility/cluster.R
+++ analyses/cluster_reproducibility/cluster.R
@@ -3,7 +3,6 @@ library(glue)
 library(tidyverse)
 library(ConsensusClusterPlus)
 library(edgeR)
-library(SPsimSeq)
 library(reticulate)
 use_condaenv("stem-base")
 source(here("src", "R", "de_analysis.R"))
@@ -16,33 +15,50 @@ env <- yte$process_yaml(paste0(
 seed <- env$seed
 
 ad <- import("anndata")
+sc <- import("scanpy")
 
 workdir <- do.call(here, as.list(env$workdir))
+sim <- do.call(here, as.list(env$outs$simulated))
 outdir <- do.call(here, as.list(env$outs$clustered))
-datasets <- here(outdir, "datasets")
 
-dir.create(outdir)
+dir.create(outdir, recursive = TRUE)
 
-adatas <- c(list.files(datasets), env$datasets$real)
+reals <- map_chr(env$datasets$real, \(x) do.call(here, as.list(x)))
+
+adatas <- c(list.files(sim, full.names = TRUE), reals)
 
 cluster_call_de <- function(file) {
   library(ConsensusClusterPlus)
 
   adata <- ad$read_h5ad(file)
+  sc$pp$filter_cells(adata, min_genes = 50)
+  sc$pp$filter_cells(adata, min_counts = 100)
+  sc$pp$filter_genes(adata, min_cells = 20)
 
-  dge <- edgeR::DGEList(adata$X)
-  rownames(dge) <- rownames(adata$var)
+  if (adata$shape[[1]] == 0) {
+    warning(glue("File {basename(file)} has no count data after filtering"))
+    return()
+  } else {
+    message(glue("Proceeding {basename(file)} to DE analysis"))
+  }
+
+  x <- adata$X
+  if (!is.matrix(x)) {
+    x <- as.matrix(x)
+    x[is.na(x)] <- 0
+  }
+  dge <- edgeR::DGEList(t(x) + 1)
+
   dge$genes$gene <- rownames(adata$var)
-  colnames(dge) <- rownames(adata$obs)
 
   dge <- edgeR::normLibSizes(dge, method = "TMM")
   expr <- edgeR::cpm(dge, log = TRUE)
 
-  result <- ConsensusClusterPlus(d = expr)
+  result <- ConsensusClusterPlus(d = expr, maxK = 7)
   ic_vals <- calcICL(result)
   best <- as_tibble(ic_vals$clusterConsensus) |>
     group_by(k) |>
-    summarise(avg = mean(ClusterConsensus, na.rm = TRUE)) |>
+    summarise(avg = mean(clusterConsensus, na.rm = TRUE)) |>
     slice_max(avg) |>
     pluck("k")
 
@@ -58,11 +74,12 @@ cluster_call_de <- function(file) {
 
   best_cc <- tb[[glue("cc_{best}")]]
 
-  dge$samples$best_cc <- best_cc
+  dge$samples$best_cc <- as.character(best_cc)
+  dge$samples$sample <- rownames(adata$obs)
   de_result <- edgeR_glm_wrapper(dge, group = "best_cc", id_col = "gene")
 
-  adata$obs <- merge(adata$obs, tb, by = "row.names")
-  adata$obs$best_cc <- best - cc
+  adata$obs <- tb
+  adata$obs$best_cc <- best_cc
   adata$uns$clusterConsensus <- as.data.frame(ic_vals$clusterConsensus)
   adata$uns$itemConsensus <- as.data.frame(ic_vals$itemConsensus)
   adata$uns$top_de <- de_result$top
@@ -70,5 +87,11 @@ cluster_call_de <- function(file) {
 }
 
 for (file in adatas) {
-  result <- cluster_call_de(file)
+  out <- here(outdir, basename(file))
+  if (!file.exists(out)) {
+    result <- cluster_call_de(file)
+    if (!is.null(result)) {
+      result$write_h5ad(out)
+    }
+  }
 }
