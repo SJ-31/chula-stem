e35954d4eb9b129949d83bf7a9cc9feefda27542
Shann Chongwattanankul <shanj3131@gmail.com>
2026-05-14 07:38:17 +0700

    chore: simplify app

--- src/R/data_portal/app.R
+++ src/R/data_portal/app.R
@@ -5,211 +5,35 @@ suppressMessages({
   library(tidyverse)
   library(googlesheets4)
   library(reactable)
+  library(crosstalk)
 })
 gs4_deauth()
 
-link <- "https://docs.google.com/spreadsheets/d/1h8aGAyhQk1IL2MALBZqmARUjvurcYIdCfi2GcHPQVc4/edit?gid=870277624#gid=870277624"
-
-
-setup_data <- function() {
-  data <- list(cohorts = list(), ttypes = list())
-  df <- read_sheet(link) |>
-    mutate(tumor_type = str_to_upper(tumor_type)) |>
-    select(-date_received) |>
-    relocate(path, .after = everything())
-  modalities <- as.character(df[["modality"]]) |> unique()
-  data$dfs <- lapply(modalities, \(x) {
-    filtered <- df |> filter(modality == x)
-    data$selections[[x]] <<- select(filtered, c("cohort", "tumor_type")) |>
-      distinct()
-    filtered |> select(-modality)
-  }) |>
-    `names<-`(modalities)
-  data
-}
-
-D <- setup_data()
-
-
-clean_modality <- function(name) {
-  str_replace_all(str_to_lower(name), "-", "_")
-}
-
-get_selection <- function(input, condition_on, out_col) {
-  choice_df <- D$selections[[clean_modality(input$nav)]]
-
-  if (input[[condition_on]] != "-") {
-    mask <- choice_df[[condition_on]] == input[[condition_on]]
-    filtered <- choice_df[mask, ]
-  } else {
-    filtered <- choice_df
-  }
-  sort(c(unique(as.character(filtered[[out_col]])), "-"))
-}
-
-rename_df <- function(df) {
-  df <- df |>
-    rename(
-      Case = "case_name",
-      Path = "path",
-      PBMC = "has_pbmc",
-      Tumor = "has_tumor",
-      Raw = "has_raw",
-      Processed = "has_processed"
-    )
-  if ("tumor_type" %in% colnames(df)) {
-    df <- df |> rename("Tumor Type" = "tumor_type")
-  }
-  if ("cohort" %in% colnames(df)) {
-    df <- df |> rename(Cohort = "cohort")
-  }
-  df
-}
-
-select_filter <- function(values, name) {
-  tags$select(
-    onchange = sprintf(
-      "Reactable.setFilter('main_tab', '%s', event.target.value || undefined)",
-      name
-    ),
-    tags$option(value = "", "All"),
-    lapply(unique(values), tags$option)
-  )
-}
-
-bool_col <- colDef(
-  filterable = TRUE,
-  filterInput = select_filter,
-  align = "center",
-  cell = \(v) {
-    if (v == "F") "\u274c" else "\u2714\ufe0f"
-  }
-)
-
-make_sample_table <- function(input) {
-  df <- D$dfs[[clean_modality(input$nav)]]
-
-  cohort_val <- input$cohort
-  ttype_val <- input$tumor_type
-
-  if (cohort_val != "-" && ttype_val != "-") {
-    filtered <- df |>
-      filter(
-        cohort == cohort_val & tumor_type == ttype_val
-      ) |>
-      select(-cohort, -tumor_type)
-  } else if (cohort_val != "-") {
-    filtered <- df |>
-      filter(cohort == cohort_val) |>
-      select(-cohort)
-  } else if (ttype_val != "-") {
-    filtered <- df |>
-      filter(tumor_type == ttype_val) |>
-      select(-tumor_type)
-  } else {
-    filtered <- df
-  }
-  reactable(
-    as.data.frame(rename_df(filtered)),
-    pagination = FALSE,
-    searchable = TRUE,
-    showPagination = TRUE,
-    resizable = TRUE,
-    wrap = FALSE,
-    columns = list(
-      PBMC = bool_col,
-      Raw = bool_col,
-      Processed = bool_col,
-      Tumor = bool_col,
-      Path = colDef(filterable = FALSE)
-    ),
-    elementId = "main_tab",
-    columnGroups = list(
-      colGroup(
-        name = "Available Data",
-        columns = c("Processed", "Raw")
-      ),
-      colGroup(
-        name = "Available Samples",
-        columns = c("Tumor", "PBMC")
-      )
-    )
-  )
-}
-
-## plot_samples <- function(df) {
-
-## }
+source("setup.R")
 
 ui <- page_navbar(
   theme = bs_theme(bootswatch = "flatly"),
   id = "nav",
-  nav_panel("Exome", reactableOutput("t1")),
-  nav_panel("RNA-seq", reactableOutput("t2")),
-  nav_panel("scRNA-seq", reactableOutput("t3")),
-  nav_panel("TCR-seq", reactableOutput("t4")),
-  nav_panel("sc-ATAC-seq", reactableOutput("t5")),
-  sidebar = sidebar(
-    selectInput(
-      inputId = "cohort",
-      selected = "-",
-      label = "Cohort",
-      choices = "-"
-    ),
-    selectInput(
-      inputId = "tumor_type",
-      selected = "-",
-      label = "Tumor Type",
-      choices = "-"
-    ),
+  nav_panel("Clinical", reactableOutput("clin_tab")),
+  nav_panel("Metadata", reactableOutput("meta_tab")),
+  nav_panel(
+    "Catalog",
+    reactableOutput("mtab")
   ),
-  title = "Modalities"
+  sidebar = sidebar(
+    h3("Filters"),
+    filter_checkbox("modality", "Modality", D$all, ~Modality),
+    filter_select("cohort", "Cohort", D$all, ~Cohort),
+    filter_checkbox("ttype", "Tumor Type", D$all, ~`Tumor Type`),
+    csv_download_button("mtab")
+  )
 )
 
 server <- function(input, output, session) {
-  output$t1 <- renderReactable(make_sample_table(input))
-  output$t2 <- renderReactable(make_sample_table(input))
-  output$t3 <- renderReactable(make_sample_table(input))
-  output$t4 <- renderReactable(make_sample_table(input))
-  output$t5 <- renderReactable(make_sample_table(input))
-  observeEvent(input$nav, {
-    updateSelectInput(
-      inputId = "cohort",
-      selected = "-",
-      choices = get_selection(input, "tumor_type", "cohort")
-    )
-    updateSelectInput(
-      inputId = "tumor_type",
-      selected = "-",
-      choices = get_selection(input, "cohort", "tumor_type")
-    )
-  })
-  observeEvent(input$cohort, {
-    choices <- get_selection(input, "cohort", "tumor_type")
-    if (input$tumor_type %notin% choices) {
-      selected <- "-"
-    } else {
-      selected <- input$tumor_type
-    }
-    updateSelectInput(
-      inputId = "tumor_type",
-      selected = selected,
-      choices = choices
-    )
-  })
-  observeEvent(input$tumor_type, {
-    choices <- get_selection(input, "tumor_type", "cohort")
-    if (input$cohort %notin% choices) {
-      selected <- "-"
-    } else {
-      selected <- input$cohort
-    }
-    updateSelectInput(
-      inputId = "cohort",
-      selected = selected,
-      choices = get_selection(input, "tumor_type", "cohort")
-    )
-  })
+  output$mtab <- renderReactable(make_sample_table())
+  output$clin_tab <- renderReactable(make_clinical_table())
+  output$meta_tab <- renderReactable(make_metadata_table())
+  selected <- reactive(getReactableState("mtab", "selected"))
 }
 
 shinyApp(ui = ui, server = server)
---
+++ src/R/data_portal/setup.R
@@ -0,0 +1,179 @@
+library(tidyverse)
+library(crosstalk)
+
+SAMPLE_SHEET <- "https://docs.google.com/spreadsheets/d/1h8aGAyhQk1IL2MALBZqmARUjvurcYIdCfi2GcHPQVc4/edit?gid=870277624#gid=870277624"
+
+OTHER_SHEET <- "https://docs.google.com/spreadsheets/d/1eNsxUPIf3X3l7W5wR4uIB-_TRWiB8yQry36zCxqlo0U/edit?gid=0#gid=0"
+
+JOIN_ON <- c(
+  "tumor_type",
+  "case_name",
+  "cohort"
+)
+
+EXPECTED_COLS <- list(
+  clinical = c("HN", "diagnosis", "note"),
+  metadata = c(
+    "platform",
+    "ngs_provider",
+    "sample_collection_date",
+    "received_date"
+  )
+)
+
+read_from_other <- function(link, sheet_name, grouped_sample_df) {
+  read_sheet(link, sheet = sheet_name) |>
+    mutate(tumor_type = str_to_upper(tumor_type)) |>
+    left_join(grouped_sample_df, by = JOIN_ON) |>
+    mutate(`Has data?` = ifelse(is.na(Modality), "F", "T")) |>
+    select(-Modality)
+}
+
+
+rename_df <- function(df) {
+  lookup <- c(
+    Case = "case_name",
+    Path = "path",
+    PBMC = "has_pbmc",
+    Tumor = "has_tumor",
+    Raw = "has_raw",
+    Processed = "has_processed",
+    Modality = "modality",
+    Diagnosis = "diagnosis",
+    `NGS platform` = "platform",
+    `NGS provider` = "ngs_provider",
+    `Sample collection` = "sample_collection_date",
+    `Received data` = "received_date",
+    `Tumor Type` = "tumor_type",
+    Cohort = "cohort",
+    Note = "note"
+  )
+  df |> rename(any_of(lookup))
+}
+
+D <- local({
+  data <- list()
+  df <- read_sheet(SAMPLE_SHEET) |>
+    mutate(
+      tumor_type = str_to_upper(tumor_type),
+      modality = replace_values(
+        modality,
+        "exome" ~ "Exome",
+        "rna_seq" ~ "RNA seq",
+        "scrna_seq" ~ "scRNA seq",
+        "sc_atac_seq" ~ "scATAC seq",
+        "tcr_seq" ~ "TCR seq"
+      ),
+    ) |>
+    select(-date_received) |>
+    relocate(path, .after = everything())
+  grouped <- df |>
+    group_by(cohort, case_name, tumor_type) |>
+    summarise(Modality = paste0(modality, collapse = "; "))
+  data$all <- rename_df(df) |> SharedData$new(group = "tables")
+  data$clinical <- read_from_other(OTHER_SHEET, "clinical", grouped) |>
+    rename_df() |>
+    SharedData$new(group = "tables")
+  data$meta <- read_from_other(OTHER_SHEET, "metadata", grouped) |>
+    rename_df() |>
+    SharedData$new(group = "tables")
+  data
+})
+
+
+select_filter <- function(values, name) {
+  tags$select(
+    onchange = sprintf(
+      "Reactable.setFilter('main_tab', '%s', event.target.value || undefined)",
+      name
+    ),
+    tags$option(value = "", "All"),
+    lapply(unique(values), tags$option)
+  )
+}
+
+bool_col <- colDef(
+  filterable = TRUE,
+  filterInput = select_filter,
+  align = "center",
+  cell = \(v) {
+    if (v == "F") "\u274c" else "\u2714\ufe0f"
+  }
+)
+
+csv_download_button <- function(
+  id,
+  filename = "data.csv",
+  label = "Download as CSV"
+) {
+  tags$button(
+    tagList(icon("download"), label),
+    onclick = sprintf("Reactable.downloadDataCSV('%s', '%s')", id, filename)
+  )
+}
+
+make_clinical_table <- function(input) {
+  reactable(
+    D$clinical,
+    searchable = TRUE,
+    wrap = FALSE,
+    columns = list(
+      Note = colDef(
+        cell = function(value, index, name) "",
+        html = TRUE,
+        details = JS(
+          "function(rowInfo) {
+        return `</br>${rowInfo.values['Note']}</br>`
+}"
+        ),
+      ),
+      `Has data?` = bool_col
+    )
+  )
+}
+
+make_metadata_table <- function() {
+  reactable(
+    D$meta,
+    columnGroups = list(
+      colGroup(
+        name = "Dates",
+        columns = c("Sample collection", "Received data")
+      )
+    ),
+    columns = list(
+      `Sample collection` = colDef(format = colFormat(date = TRUE)),
+      `Received data` = colDef(format = colFormat(date = TRUE))
+    )
+  )
+}
+
+
+make_sample_table <- function() {
+  reactable(
+    D$all,
+    pagination = FALSE,
+    searchable = TRUE,
+    showPagination = TRUE,
+    resizable = TRUE,
+    wrap = FALSE,
+    columns = list(
+      PBMC = bool_col,
+      Raw = bool_col,
+      Processed = bool_col,
+      Tumor = bool_col,
+      Path = colDef(filterable = FALSE)
+    ),
+    elementId = "main_tab",
+    columnGroups = list(
+      colGroup(
+        name = "Available Data",
+        columns = c("Processed", "Raw")
+      ),
+      colGroup(
+        name = "Available Samples",
+        columns = c("Tumor", "PBMC")
+      )
+    )
+  )
+}
