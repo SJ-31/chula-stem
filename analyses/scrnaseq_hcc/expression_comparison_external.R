a51b293456657654f0cb733790dc54126f9954f9
Shann Chongwattanankul <shanj3131@gmail.com>
2026-07-02 12:13:15 +0700

    chore: update import script

--- analyses/scrnaseq_hcc/expression_comparison_external.R
+++ analyses/scrnaseq_hcc/expression_comparison_external.R
@@ -4,7 +4,7 @@ suppressMessages({
   library(ggplot2)
   library(tidyverse)
   options(box.path = here("src"))
-  box::use(R / plotting[dotplot])
+  suppressWarnings(box::use(R / plotting[dotplot]))
   use_condaenv("stem-base")
   ad <- import("anndata")
 })
@@ -24,6 +24,7 @@ combined_file <- here(data_dir, "combined.h5ad")
 
 ## * Collect misc. data
 
+## ** GSE154883
 gse154883_file <- here(geo, "GSE154883", "adata.h5ad")
 if (!file.exists(gse154883_file)) {
   library(Seurat)
@@ -60,48 +61,87 @@ if (!file.exists(gse154883_file)) {
   gse154883$write_h5ad(gse154883_file)
 }
 
+## ** GSE264261
+
+gse264261_file <- here(geo, "GSE264261", "adata.h5ad")
+if (!exists()) {
+  sc <- import("scanpy")
+  adata <- sc$read_10x_h5(here(
+    geo,
+    "GSE264261",
+    "GSM8215174_filtered_feature_bc_matrix.h5"
+  ))
+  obs <- read_tsv(here(geo, "GSE264261", "GSM8215174_clusters.tsv.gz")) |>
+    column_to_rownames(var = "barcode")
+  adata$obs <- merge(adata$obs, obs, by = "row.names") |>
+    column_to_rownames("Row.names")
+  adata <- adata[adata$obs$status != "doublet", ]
+  adata$obs$sample <- ifelse(
+    adata$obs$assignment == 0,
+    "GSE264261_cluster0",
+    "GSE264261_cluster1"
+  )
+  adata$obs <- select(adata$obs, sample)
+  adata$write_h5ad(gse264261_file)
+}
+
 ## * Plotting
 
-wanted_genes <- c(
-  "BSG", # CD147
-  "LIN28A",
-  "LIN28B",
-  "GPC3", # glypican 3
-  "AFP" # Alpha-fetoprotein
+wanted_genes <- list(
+  main = c(
+    "BSG", # CD147
+    "LIN28A",
+    "LIN28B",
+    "GPC3", # glypican 3
+    "AFP"
+  ), # Alpha-fetoprotein
+  edgeR_DE = c(
+    # Genes DE with edgeR
+    "SDF2L1",
+    "NUDT14",
+    "SCAND1",
+    "MRPL55",
+    "RPL28",
+    "C4orf48",
+    "HCFC1R1",
+    "ABHD17A",
+    "GRINA",
+    "MEA1"
+  ),
+  scVI_DE = c(
+    "GAL",
+    "TAGLN",
+    "MYL9",
+    "RFLNB"
+  )
 )
 
+group_labels <- list(
+  "type" = "Redmonder::qMSOBu",
+  "source" = "pals::glasbey"
+)
 
 combined <- ad$read_h5ad(combined_file)
 
-norm_plot <- dotplot(
+save_dot <- function(plot, fname) {
+  ggsave(fname, width = 12, height = 9, plot = plot)
+}
+
+dotplot(
   obj = combined,
   var_names = wanted_genes,
   group_by = "sample",
-  group_labels = list(
-    "type" = "Redmonder::qMSOBu",
-    "source" = "pals::glasbey"
-  ),
+  group_labels = group_labels,
   layer = "x_norm",
   sort = "type"
-)
-ggsave(
-  here(data_dir, "dotplot_normalized.pdf"),
-  width = 12,
-  plot = norm_plot
-)
+) |>
+  save_dot(here(data_dir, "dotplot_normalized.pdf"))
 
-raw_plot <- dotplot(
+dotplot(
   obj = combined,
   var_names = wanted_genes,
   group_by = "sample",
-  group_labels = list(
-    "type" = "Redmonder::qMSOBu",
-    "source" = "pals::glasbey"
-  ),
+  group_labels = group_labels,
   sort = "type"
-)
-ggsave(
-  here(data_dir, "dotplot_raw.pdf"),
-  width = 12,
-  plot = raw_plot
-)
+) |>
+  save_dot(here(data_dir, "dotplot_raw.pdf"))
